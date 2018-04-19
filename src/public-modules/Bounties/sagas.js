877fee1c265e929734d50aeae8580c45225c3bee
Wilson Yu <hello@wilsonyu.io>
2018-04-19 05:22:56 -0400

    refactor sagas into separate files

--- src/public-modules/Bounties/sagas.js
+++ src/public-modules/Bounties/sagas.js
@@ -19,25 +19,4 @@ export function* watchBounties() {
   yield takeLatest(LOAD_BOUNTIES, loadBounties);
 }
 
-// STATS
-//
-//
-// Load Profile Stats: bounties, accepted bounties, acceptance rate, submissions data
-export function* loadProfileStats(action) {
-  const address = action.address;
-  try {
-    let endpoint = `/stats/profile/${address}`;
-    const profileStats = yield call(request, endpoint, 'GET');
-    yield put(loadProfileStatsSuccess(profileStats));
-  } catch (e) {
-    yield put(loadProfileStatsFail(e));
-  }
-}
-
-export function* watchProfileStats() {
-  yield takeLatest(LOAD_PROFILESTATS, loadProfileStats);
-}
-
-// Load Stats regarding CREATING bounties: draft/active/dead/completed/expired
-
 export default [watchBounties];
