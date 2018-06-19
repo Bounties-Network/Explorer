cd66844b0afbe1f10eb904e05dc0a4b72cb5c05d
William Villanueva <wvillanu89@gmail.com>
2018-06-19 13:03:32 +0200

    Allow for router elements to be rendered in storyboard.

--- src/stories/Sidebar.stories.js
+++ src/stories/Sidebar.stories.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { MemoryRouter } from 'react-router-dom';
 
 import { Sidebar } from 'components';
 
-storiesOf('Sidebar', module).add('Sidebar', () => (
-  <div>
-    <Sidebar onClick={console.log} />
-  </div>
-));
+storiesOf('Sidebar', module)
+  .addDecorator(story => (
+    <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
+  ))
+  .add('Sidebar', () => (
+    <div>
+      <Sidebar onClick={console.log} />
+    </div>
+  ));
