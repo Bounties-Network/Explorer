b715b41eb1ce360aecf9046a7d15c2451e99abdb
William Villanueva <wvillanu89@gmail.com>
2018-06-26 15:18:21 +0200

    Initialized generic dropdown component.

---
+++ src/components/Dropdown/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Dropdown.module.scss';
+
+const Dropdown = props => {
+  return <div>dropdown</div>;
+};
+
+Dropdown.propTypes = {};
+
+Dropdown.defaultProps = {};
+
+export default Dropdown;
