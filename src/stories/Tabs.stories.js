dedc18dd63cf1a73d7c17e87f128a5c932dc6385
William Villanueva <wvillanu89@gmail.com>
2018-06-30 23:21:28 +0200

    Rebuilt tab and tabs components from scratch.

--- src/stories/Tabs.stories.js
+++ src/stories/Tabs.stories.js
@@ -5,14 +5,16 @@ import { action } from '@storybook/addon-actions';
 
 import { Tabs } from 'components';
 
-const tabsData = [
-  { title: 'Active', tabCount: 3, active: true },
-  { title: 'Pending', tabCount: 24 },
-  { title: 'Completed', tabCount: 0 }
-];
-
 storiesOf('Tabs', module).add('Tabs', () => (
   <div>
-    <Tabs tabs={tabsData} onClick={action('clicked')} />
+    <Tabs onSelect={() => {}}>
+      <Tabs.Tab tabColor="blue" eventKey={1}>
+        Active
+      </Tabs.Tab>
+      <Tabs.Tab tabColor="green" eventKey={2}>
+        Pending
+      </Tabs.Tab>
+      <Tabs.Tab eventKey={3}>Completed</Tabs.Tab>
+    </Tabs>
   </div>
 ));
