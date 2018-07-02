86b1c3beafacc69d5741f75c4306022606305530
William Villanueva <wvillanu89@gmail.com>
2018-07-02 18:57:36 +0200

    Refactored Sidebar component added with full stories. :)

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Sidebar.module.scss';
+import { Link } from 'react-router-dom';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+
+const ModalContext = React.createContext({});
+
+class TabIcon extends React.Component {
+  render() {
+    return (
+      <ModalContext.Consumer>
+        {({ activeTab, onTabClick }) => {
+          const { icon, link, tabKey } = this.props;
+          let tabStyle = styles.iconTab;
+
+          if (activeTab === tabKey) {
+            tabStyle += ` ${styles.active}`;
+          }
+
+          return (
+            <a
+              className={tabStyle}
+              to={link}
+              onClick={() => onTabClick(tabKey)}
+            >
+              <FontAwesomeIcon icon={icon} />
+            </a>
+          );
+        }}
+      </ModalContext.Consumer>
+    );
+  }
+}
+
+TabIcon.propTypes = {
+  icon: PropTypes.array,
+  link: PropTypes.string,
+  tabKey: PropTypes.string
+};
+
+class Sidebar extends React.Component {
+  state = {
+    activeTab: null
+  };
+
+  onTabClick = tabKey => {
+    this.setState({ activeTab: tabKey });
+  };
+
+  render() {
+    const { activeTab } = this.state;
+    const { defaultActiveTab } = this.props;
+
+    return (
+      <ModalContext.Provider
+        value={{
+          activeTab: activeTab || defaultActiveTab,
+          onTabClick: this.onTabClick
+        }}
+      >
+        <div className={`${styles.sidebar}`}>{this.props.children}</div>
+      </ModalContext.Provider>
+    );
+  }
+}
+
+Sidebar.propTypes = {
+  children: PropTypes.arrayOf(function(propValue, key) {
+    for (let i = 0; i < propValue.length; i++) {
+      if (propValue[i].type.name !== TabIcon.name) {
+        return new Error('Children Must Be an Instance of TabIcon');
+      }
+    }
+  }),
+  defaultActiveTab: PropTypes.string
+};
+
+Sidebar.defaultProps = {};
+Sidebar.TabIcon = TabIcon;
+
+export default Sidebar;
--- src/fontAwesome.js
+++ src/fontAwesome.js
@@ -4,5 +4,19 @@ import cog from '@fortawesome/fontawesome-pro-light/faCog';
 import faTimes from '@fortawesome/fontawesome-pro-light/faTimes';
 import wallet from '@fortawesome/fontawesome-pro-light/faWallet';
 import signout from '@fortawesome/fontawesome-pro-light/faSignOut';
+import tachometer from '@fortawesome/fontawesome-pro-light/faTachometer';
+import listAlt from '@fortawesome/fontawesome-pro-light/faListAlt';
+import trophyAlt from '@fortawesome/fontawesome-pro-light/faTrophyAlt';
+import userAlt from '@fortawesome/fontawesome-pro-light/faUserAlt';
 
-library.add(faBell, cog, signout, faTimes, wallet);
+library.add(
+  faBell,
+  cog,
+  signout,
+  faTimes,
+  wallet,
+  tachometer,
+  listAlt,
+  trophyAlt,
+  userAlt
+);
