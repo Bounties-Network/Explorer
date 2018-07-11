12edd6a59ed4aa6deebdad0941d53b03e5e3377a
Corwin Harrell <hello@corwinharrell.com>
2018-07-11 11:06:42 -0400

    Refine styles for listGroup component

--- src/stories/ListGroup.stories.js
+++ src/stories/ListGroup.stories.js
@@ -9,11 +9,12 @@ storiesOf('ListGroup', module).add('ListGroup', () => (
   <div>
     <div style={{ marginTop: '10px' }} />
     <Text type="H3">Basic List Group</Text>
-    <div style={{ marginTop: '10px' }} />
-    <ListGroup>
-      <ListGroup.ListItem hover>List Item 1</ListGroup.ListItem>
-      <ListGroup.ListItem hover>List Item 2</ListGroup.ListItem>
-      <ListGroup.ListItem hover>List Item 3</ListGroup.ListItem>
-    </ListGroup>
+    <div style={{ margin: '50px' }}>
+      <ListGroup>
+        <ListGroup.ListItem hover>List Item 1</ListGroup.ListItem>
+        <ListGroup.ListItem hover>List Item 2</ListGroup.ListItem>
+        <ListGroup.ListItem hover>List Item 3</ListGroup.ListItem>
+      </ListGroup>
+    </div>
   </div>
 ));
