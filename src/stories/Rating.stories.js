59a7b98f480f907b9c8261513d3f1ae9b1c70fc3
Corwin Harrell <hello@corwinharrell.com>
2018-07-11 10:40:24 -0400

    Refine and refactor rating component styles

--- src/stories/Rating.stories.js
+++ src/stories/Rating.stories.js
@@ -8,7 +8,7 @@ import { Rating, Text } from 'components';
 storiesOf('Rating', module).add('Rating', () => (
   <div style={{ marginLeft: '20px' }}>
     <div style={{ marginTop: '10px' }} />
-    <Text type="H3">Rating Component with Default</Text>
+    <Text type="H3">Rating Component with Default Value</Text>
     <div style={{ marginTop: '10px' }} />
     <Rating defaultValue={4} />
   </div>
