77efbf0864ef32cb407a12f89a9a44d142584323
William Villanueva <wvillanu89@gmail.com>
2018-07-13 11:56:43 +0000

    Added required actions, reducer functionality around filters, categories, and sorts

--- src/public-modules/Bounties/index.js
+++ src/public-modules/Bounties/index.js
@@ -7,7 +7,21 @@ const initialState = {
   offset: 0,
   count: 0,
   sort: SORT_VALUE,
-  searchOptions: {},
+  sort_order: 'asc',
+  search: '',
+  stage_filters: {
+    drafts: false,
+    active: false,
+    completed: false,
+    expired: false,
+    dead: false
+  },
+  difficulty_filter: {
+    beginner: false,
+    intermediate: false,
+    advanced: false
+  },
+  category_filters: new Set([]),
   bounties: []
 };
 
@@ -15,14 +29,8 @@ const LOAD_BOUNTIES = 'bounties/LOAD_BOUNTIES';
 const LOAD_BOUNTIES_SUCCESS = 'bounties/LOAD_BOUNTIES_SUCCESS';
 const LOAD_BOUNTIES_FAIL = 'bounties/LOAD_BOUNTIES_FAIL';
 
-const LOAD_MORE = 'bounties/LOAD_MORE';
-
-function loadMoreBounties() {
-  return { type: LOAD_MORE };
-}
-
-function loadBounties(searchOptions) {
-  return { type: LOAD_BOUNTIES, searchOptions };
+function loadBounties() {
+  return { type: LOAD_BOUNTIES };
 }
 
 function loadBountiesSuccess(bounties) {
@@ -37,8 +45,100 @@ function loadBountiesFail(error) {
   return { type: LOAD_BOUNTIES_FAIL, error };
 }
 
+const LOAD_MORE = 'bounties/LOAD_MORE';
+
+function loadMoreBounties() {
+  return { type: LOAD_MORE };
+}
+
+const SET_SORT = 'bounties/SET_SORT';
+const SET_SEARCH = 'bounties/SET_SEARCH';
+const TOGGLE_STAGE_FILTER = 'bounties/TOGGLE_STAGE_FILTER';
+const TOGGLE_DIFFICULTY_FILTER = 'bounties/TOGGLE_DIFFICULTY_FILTER';
+const ADD_CATEGORY_FILTER = 'bounties/SET_CATEGORY_FILTER';
+const REMOVE_CATEGORY_FILTER = 'bounties/REMOVE_CATEGORY_FILTER';
+
+function setSort(sort, sortOrder) {
+  return { type: SET_SORT, sort, sortOrder };
+}
+
+function setSearch(search) {
+  return { type: SET_SEARCH, search };
+}
+
+function toggleStageFilter(stage) {
+  return { type: TOGGLE_STAGE_FILTER, stage };
+}
+
+function toggleDifficultyFilter(difficulty) {
+  return { type: TOGGLE_DIFFICULTY_FILTER, difficulty };
+}
+
+function addCategoryFilter(category) {
+  return { type: ADD_CATEGORY_FILTER, category };
+}
+
+function removeCategoryFilter(category) {
+  return { type: REMOVE_CATEGORY_FILTER, category };
+}
+
 function BountiesReducer(state = initialState, action) {
   switch (action.type) {
+    case ADD_CATEGORY_FILTER: {
+      const { category } = action;
+      const updated_filters = new Set(state.category_filters);
+      updated_filters.add(category);
+
+      return {
+        ...state,
+        category_filters: updated_filters
+      };
+    }
+    case REMOVE_CATEGORY_FILTER: {
+      const { category } = action;
+      const updated_filters = new Set(state.category_filters);
+      updated_filters.delete(category);
+
+      return {
+        ...state,
+        category_filters: updated_filters
+      };
+    }
+    case TOGGLE_DIFFICULTY_FILTER: {
+      const { difficulty } = action;
+      return {
+        ...state,
+        difficulty_filters: {
+          ...state.difficulty_filters,
+          [difficulty]: !state.difficulty_filtersl[difficulty]
+        }
+      };
+    }
+    case TOGGLE_STAGE_FILTER: {
+      const { stage } = action;
+      return {
+        ...state,
+        stage_filters: {
+          ...state.stage_filters,
+          [stage]: !state.stage_filters[stage]
+        }
+      };
+    }
+    case SET_SEARCH: {
+      const { search } = action;
+      return {
+        ...state,
+        search
+      };
+    }
+    case SET_SORT: {
+      const { sort, sortOrder } = action;
+      return {
+        ...state,
+        sort,
+        sortOrder
+      };
+    }
     case LOAD_BOUNTIES: {
       const { searchOptions } = action;
       return {
