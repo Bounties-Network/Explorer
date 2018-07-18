3104a0412b4ca3dc9a22c6353a88bf155497a2a7
William Villanueva <wvillanu89@gmail.com>
2018-07-18 14:27:42 +0400

    Auth selectors required set up.

--- src/public-modules/Authentication/selectors.js
+++ src/public-modules/Authentication/selectors.js
@@ -1,3 +1,23 @@
 import { createSelector } from 'reselect';
 
 export const rootAuthSelector = state => state.authentication;
+
+export const loadNonceStateSelector = createSelector(
+  rootAuthSelector,
+  rootAuth => rootAuth.loadNonceState
+);
+
+export const loginStateSelector = createSelector(
+  rootAuthSelector,
+  rootAuth => rootAuth.loginState
+);
+
+export const getCurrentUserStateSelector = createSelector(
+  rootAuthSelector,
+  rootAuth => rootAuth.getCurrentUserState
+);
+
+export const getCurrentUser = createSelector(
+  rootAuthSelector,
+  rootAuth => rootAuth.user
+);
