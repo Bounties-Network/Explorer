b3ea985745f292716968c934bcd10eb20387c6e1
William Villanueva <wvillanu89@gmail.com>
2018-07-18 10:58:46 +0400

    Update sidenav to work with the new styles/component.

---
+++ src/layout/App/constants.js
@@ -0,0 +1,22 @@
+export const NAV_ITEMS = [
+  {
+    icon: ['far', 'tachometer'],
+    to: '/dashboard',
+    tabKey: 'dashboard'
+  },
+  {
+    icon: ['far', 'list-alt'],
+    to: '/explorer',
+    tabKey: 'explorer'
+  },
+  {
+    icon: ['far', 'trophy-alt'],
+    to: '/leaderboard',
+    tabKey: 'leaderboard'
+  },
+  {
+    icon: ['far', 'user-alt'],
+    to: '/profile',
+    tabKey: 'profile'
+  }
+];
--- src/layout/App/index.js
+++ src/layout/App/index.js
@@ -4,8 +4,9 @@ import styles from './App.module.scss';
 import { hot } from 'react-hot-loader';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { compose } from 'redux';
+import { map } from 'lodash';
 import { NoMatch } from 'layout';
-
+import { NAV_ITEMS } from './constants';
 import { Explorer } from 'containers';
 import { Sidebar } from 'components';
 import { Header } from 'layout';
@@ -14,13 +15,21 @@ import '../../styles/flexboxgrid.css';
 import '../../font-files/inter-ui.css';
 
 class AppComponent extends React.Component {
+  renderSideNavItems() {
+    return map(navItem => {
+      return <Sidebar.TabIcon {...navItem} key={navItem.tabKey} />;
+    }, NAV_ITEMS);
+  }
+
   render() {
     const { network } = this.props;
 
     return (
       <div className={styles.app}>
         <Header />
-        <Sidebar />
+        <Sidebar defaultActiveTab="dashboard" className={styles.sideNav}>
+          {this.renderSideNavItems()}
+        </Sidebar>
         <div className={`${styles.body}`}>
           <Switch>
             <Route exact path="/" component={Explorer} />
