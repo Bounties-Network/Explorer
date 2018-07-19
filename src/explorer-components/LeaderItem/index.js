4307430f73f7ea06f809da91531dac46932b6b83
Matt Garnett <matt@mattgarnett.com>
2018-07-19 17:59:26 -0500

    convert leaderboard to using a list group instead of cards

---
+++ src/explorer-components/LeaderItem/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './LeaderItem.module.scss';
+import { map, includes } from 'lodash';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import { ListGroup, Card, Text, Pill, Avatar } from 'components';
+
+const LeaderItem = props => {
+  const { place, img, name, address, value, usd, currency } = props;
+
+  return (
+    <ListGroup.ListItem hover>
+      <div className="row">
+        <div className="col-xs-1">
+          <div className={styles.place}>
+            <Text color="defaultGrey" typeScale="h1">
+              {place}
+            </Text>
+          </div>
+        </div>
+
+        <div className="col-xs-7">
+          <div className={styles.avatar}>
+            <Avatar
+              img={img}
+              name={name}
+              address={address}
+              hash={address}
+              size="small"
+            />
+          </div>
+        </div>
+
+        <div className="col-xs-4">
+          <div className={styles.price}>
+            <Text color="purple" typeScale="h3" className={styles.usd}>
+              ${usd}
+            </Text>
+            <Text
+              color="defaultGrey"
+              typeScale="h5"
+              className={styles.currency}
+            >{`${value} ${currency}`}</Text>
+          </div>
+        </div>
+      </div>
+    </ListGroup.ListItem>
+  );
+};
+
+LeaderItem.propTypes = {
+  place: PropTypes.number,
+  img: PropTypes.string,
+  address: PropTypes.string,
+  value: PropTypes.string,
+  usd: PropTypes.string,
+  currency: PropTypes.string
+};
+
+LeaderItem.defaultProps = {};
+
+export default LeaderItem;
