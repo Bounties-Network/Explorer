6b234b5d6883626cf20622ed648efb980c277322
Matt Garnett <matt@mattgarnett.com>
2018-07-22 10:51:58 -0500

    added bounties to profile page

--- src/containers/Profile/index.js
+++ src/containers/Profile/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ProfileDetails from './ProfileDetails';
+import ProfileBounties from './ProfileBounties';
 import styles from './Profile.module.scss';
 
 const Profile = props => {
@@ -7,6 +8,16 @@ const Profile = props => {
     <div className="fullHeight">
       <div className={`${styles.profileDetails}`}>
         <ProfileDetails />
+      </div>
+      <div className={styles.profileBounties}>
+        <div className="row fullHeight">
+          <div className={`col-xs-3 fullHeight ${styles.filterNav}`}>
+            <div />
+          </div>
+          <div className={`col-xs-9 fullHeight ${styles.explorerBody}`}>
+            <ProfileBounties />
+          </div>
+        </div>
       </div>
     </div>
   );
