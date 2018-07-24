2f4bbc476f0ca8b8e055fbb13a047ced12bb868e
William Villanueva <wvillanu89@gmail.com>
2018-07-24 17:57:55 +0900

    Refactored create bounty form to use the new FormSection components for standardized behavior.

--- src/containers/CreateBounty/index.js
+++ src/containers/CreateBounty/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './CreateBounty.module.scss';
-import { PageCard } from 'explorer-components';
-import { Text, TextInput } from 'components';
+import { PageCard, FormSection } from 'explorer-components';
+import { Text, TextInput, MarkdownEditor } from 'components';
 
 const CreateBounty = props => {
   return (
@@ -10,31 +10,32 @@ const CreateBounty = props => {
         <PageCard.Title>Create Bounty</PageCard.Title>
       </PageCard.Header>
       <PageCard.Content className={styles.cardContent}>
-        <div className="row">
-          <div className="col-xs-3">
-            <Text
-              color="defaultGrey"
-              typeScale="Body"
-              weight="fontWeight-medium"
-            >
-              ABOUT
-            </Text>
-          </div>
-          <div className="col-xs-9">
-            <Text typeScale="H4" weight="fontWeight-bold">
+        <FormSection>
+          <FormSection.Section title="ABOUT">
+            <FormSection.Description>
               Enter your details about this bounty.
-            </Text>
-            <Text className={styles.subText} color="defaultGrey">
+            </FormSection.Description>
+            <FormSection.SubText>
               Enter a title and description for your bounty. A markdown preview
               will automatically be generated as you type, which you can view by
               clicking the preview button.
-            </Text>
-            <div className={styles.inputBlock}>
-              <TextInput label="Title" placeholder="Enter title..." />
-            </div>
-          </div>
-        </div>
-        <div className="row" />
+            </FormSection.SubText>
+            <FormSection.InputGroup>
+              <TextInput label="Title" placeholder="Enter Title..." />
+            </FormSection.InputGroup>
+            <FormSection.InputGroup>
+              <div className={styles.markdownEditor}>
+                <MarkdownEditor label="Description" />
+              </div>
+            </FormSection.InputGroup>
+          </FormSection.Section>
+          <FormSection.Section title="CONTACT">
+            <FormSection.Description>
+              Who will be the primary contact for bounty questions and
+              submissions?
+            </FormSection.Description>
+          </FormSection.Section>
+        </FormSection>
       </PageCard.Content>
     </PageCard>
   );
