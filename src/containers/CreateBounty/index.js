9f9b3bd9b490c0b7b98834ccc0a1e14c365041c8
William Villanueva <wvillanu89@gmail.com>
2018-07-24 18:58:34 +0900

    Fill out sections for the create bounty form.

--- src/containers/CreateBounty/index.js
+++ src/containers/CreateBounty/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import styles from './CreateBounty.module.scss';
 import { PageCard, FormSection } from 'explorer-components';
-import { Text, TextInput, MarkdownEditor } from 'components';
+import {
+  Text,
+  TextInput,
+  MarkdownEditor,
+  SearchSelect,
+  NumberInput,
+  DatePicker
+} from 'components';
 
 const CreateBounty = props => {
   return (
@@ -34,6 +41,104 @@ const CreateBounty = props => {
               Who will be the primary contact for bounty questions and
               submissions?
             </FormSection.Description>
+            <FormSection.InputGroup>
+              <div className="row">
+                <div className="col-xs-6">
+                  <TextInput label="Contact name" placeholder="Enter name..." />
+                </div>
+                <div className="col-xs-6">
+                  <TextInput
+                    label="Contact email"
+                    placeholder="Enter email..."
+                  />
+                </div>
+              </div>
+            </FormSection.InputGroup>
+          </FormSection.Section>
+          <FormSection.Section title="DETAILS">
+            <FormSection.Description>
+              How should this bounty be classified?
+            </FormSection.Description>
+            <FormSection.SubText>
+              Enter the categories and difficulty level for the bounty. Since
+              difficulty can be fairly subjective, it is helpful to provide more
+              details around required experience within your bounty description.
+            </FormSection.SubText>
+            <FormSection.InputGroup>
+              <div className="row">
+                <div className="col-xs-6">
+                  <SearchSelect label="Bounty category" />
+                </div>
+                <div className="col-xs-6">
+                  TODO - need to rewrite radio component
+                </div>
+              </div>
+            </FormSection.InputGroup>
+          </FormSection.Section>
+          <FormSection.Section title="REVISIONS">
+            <FormSection.Description>
+              Will you require revisions?
+            </FormSection.Description>
+            <FormSection.SubText>
+              Enter the maximum number of revisions you may require for this
+              task, in order to help set expectations for the contributors.
+            </FormSection.SubText>
+            <FormSection.InputGroup>
+              <NumberInput />
+            </FormSection.InputGroup>
+          </FormSection.Section>
+          <FormSection.Section title="DEADLINE">
+            <FormSection.Description>
+              When will this bounty be due?
+            </FormSection.Description>
+            <FormSection.SubText>
+              Enter the date and time for this bounty's deadline
+            </FormSection.SubText>
+            <FormSection.InputGroup>
+              <DatePicker />
+            </FormSection.InputGroup>
+          </FormSection.Section>
+          <FormSection.Section title="PAYOUT">
+            <FormSection.Description>
+              Select payout method and amount.
+            </FormSection.Description>
+            <FormSection.SubText>
+              Select the token and enter the amount you will award for
+              completion of this bounty.
+            </FormSection.SubText>
+            <FormSection.InputGroup>
+              <div className="row">
+                <div className="col-xs-4">Radio Group - needs rewrite</div>
+                <div className="col-xs-8">
+                  <TextInput
+                    label="Payout amount (ETH or whole tokens)"
+                    placeholder="Enter amount..."
+                  />
+                </div>
+              </div>
+            </FormSection.InputGroup>
+          </FormSection.Section>
+          <FormSection.Section title="SAVE OR SUBMIT">
+            <FormSection.Description>
+              When would you like to submit and activate the bounty?
+            </FormSection.Description>
+            <FormSection.SubText>
+              If you wish to activate the bounty later, you can save it as a
+              draft. The requirements for a bounty can only be edited while it
+              is in the draft stage. At minimum, your deposit amount must match
+              your payout amount.
+            </FormSection.SubText>
+            <FormSection.InputGroup>
+              <div className="row">
+                <div className="col-xs-4">Radio Group - needs rewrite</div>
+                <div className="col-xs-8">
+                  <TextInput
+                    label="Deposit amount (ETH or whole tokens)"
+                    placeholder="Enter amount..."
+                  />
+                </div>
+              </div>
+            </FormSection.InputGroup>
           </FormSection.Section>
         </FormSection>
       </PageCard.Content>
