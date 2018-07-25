038e11a724bee813f301648c7ece0e31cb165f08
William Villanueva <wvillanu89@gmail.com>
2018-07-25 16:10:41 +0900

    All validations completed and plugged in. Deposit amount disappears if activate later is set.

--- src/form-components/index.js
+++ src/form-components/index.js
@@ -1,5 +1,14 @@
 import { FormInput } from 'hocs';
-import { TextInput, MarkdownEditor } from 'components';
+import {
+  TextInput,
+  MarkdownEditor,
+  SearchSelect,
+  RadioGroup,
+  NumberInput
+} from 'components';
 
 export const FormTextInput = FormInput(TextInput);
 export const FormMarkdownEditor = FormInput(MarkdownEditor);
+export const FormSearchSelect = FormInput(SearchSelect);
+export const FormRadioGroup = FormInput(RadioGroup);
+export const FormNumberInput = FormInput(NumberInput);
