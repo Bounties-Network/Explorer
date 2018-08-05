a2aa0fa18b9f895df47692cc66b096509f76a4fb
William Villanueva <wvillanu89@gmail.com>
2018-08-06 03:16:03 +0900

    Updated social component to auto-link to page. Updated text for download prop.

--- src/components/Social/index.js
+++ src/components/Social/index.js
@@ -5,29 +5,33 @@ import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import { Text } from 'components';
 
 const Social = props => {
-  const { twitterLink, facebookLink, redditLink } = props;
-
   return (
     <div className={styles.social}>
       <Text color="grey" type="H2">
-        <a href={twitterLink}>
+        <a
+          href={`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
+            window.location.href
+          )}`}
+        >
           <FontAwesomeIcon icon={['fab', 'twitter']} className={styles.icon} />
         </a>
-        <a href={facebookLink}>
+        <a
+          href={`https://twitter.com/home?status=${encodeURIComponent(
+            window.location.href
+          )}`}
+        >
           <FontAwesomeIcon icon={['fab', 'facebook']} className={styles.icon} />
         </a>
-        <a href={redditLink}>
+        <a
+          href={`http://reddit.com/submit?url=${encodeURIComponent(
+            window.location.href
+          )}`}
+        >
           <FontAwesomeIcon icon={['fab', 'reddit']} />
         </a>
       </Text>
     </div>
   );
-};
-
-Social.propTypes = {
-  twitterLink: PropTypes.string,
-  facebookLink: PropTypes.string,
-  redditLink: PropTypes.string
 };
 
 export default Social;
