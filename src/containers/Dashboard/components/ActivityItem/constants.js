a0a65e21287592814dbb90c750a5ad0b26c45a29
Matt Garnett <matt@mattgarnett.com>
2018-08-14 18:32:20 -0500

    improve notification constants

--- src/containers/Dashboard/components/ActivityItem/constants.js
+++ src/containers/Dashboard/components/ActivityItem/constants.js
@@ -1,3 +1,5 @@
+import { NOTIFICATION_ID as nid } from 'utils/constants';
+
 const plus = ['far', 'plus-circle'];
 const level_up = ['far', 'level-up'];
 const star = ['far', 'star'];
@@ -5,87 +7,87 @@ const comment = ['far', 'comment'];
 const general = ['far', 'star'];
 
 export const notification_template = {
-  FulfillmentSubmitted: {
+  [nid.FULFILLMENT_SUBMITTED]: {
     message: 'You made a submission to',
     icon: level_up
   },
-  FulfillmentSubmittedIssuer: {
+  [nid.FULFILLMENT_SUBMITTED_ISSUER]: {
     message: 'You received a submission',
     icon: level_up
   },
-  BountyActivated: {
+  [nid.BOUNTY_ACTIVATED]: {
     message: 'You posted a new bounty',
     icon: plus
   },
-  FulfillmentAccepted: {
+  [nid.FULFILLMENT_ACCEPTED]: {
     message: 'You accepted a submission',
     icon: level_up
   },
-  FulfillmentAcceptedFulfiller: {
+  [nid.FULFILLMENT_ACCEPTED_FULFILLER]: {
     message: 'Your submission was accepted',
     icon: level_up
   },
-  BountyExpired: {
+  [nid.BOUNTY_EXPIRED]: {
     message: 'Your bounty expired',
     icon: general
   },
-  BountyIssued: {
+  [nid.BOUNTY_ISSUED]: {
     message: 'You issued a new bounty',
     icon: plus
   },
-  BountyKilled: {
+  [nid.BOUNTY_KILLED]: {
     message: 'You killed a bounty',
     icon: general
   },
-  ContributionAdded: {
+  [nid.CONTRIBUTION_ADDED]: {
     message: 'You made a contribution',
     icon: level_up
   },
-  DeadlineExtended: {
+  [nid.DEADLINE_EXTENDED]: {
     message: 'You extended the deadline',
     icon: general
   },
-  BountyChanged: {
+  [nid.BOUNTY_CHANGED]: {
     message: 'You updated a draft',
     icon: general
   },
-  IssuerTransferred: {
+  [nid.ISSUER_TRANSFERRED]: {
     message: 'You transferred ownernship',
     icon: general
   },
-  TransferRecipient: {
+  [nid.TRANSFER_RECIPIENT]: {
     message: 'A bounty was transferred to you',
     icon: plus
   },
-  PayoutIncreased: {
+  [nid.PAYOUT_INCREASED]: {
     message: 'You increased the payout',
     icon: general
   },
-  BountyComment: {
+  [nid.BOUNTY_COMMENT]: {
     message: 'You commented on',
     icon: comment
   },
-  BountyIssuedActivated: {
+  [nid.BOUNTY_ISSUED_ACTIVATED]: {
     message: 'You posted a new bounty',
     icon: plus
   },
-  FulfillmentUpdated: {
+  [nid.FULFILLMENT_UPDATED]: {
     message: 'You updated your submission to',
     icon: level_up
   },
-  FulfillmentUpdatedIssuer: {
+  [nid.FULFILLMENT_UPDATED_ISSUER]: {
     message: 'A submission for your bounty was updated:',
     icon: general
   },
-  RatingIssued: {
+  [nid.RATING_ISSUED]: {
     message: 'You wrote a review:',
     icon: star
   },
-  RatingReceived: {
+  [nid.RATING_RECEIVED]: {
     message: 'You received a rating:',
     icon: star
   },
-  ProfileUpdated: {
+  [nid.PROFILE_UPDATED]: {
     message: 'You updated your profile',
     icon: level_up
   }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -34,53 +34,29 @@ A correct submission will:
 We will require at most 3 revisions for submitted work
 `;
 
-export const FULFILLMENT_SUBMITTED = 0;
-export const FULFILLMENT_SUBMITTED_ISSUER = 1;
-export const BOUNTY_ACTIVATED = 2;
-export const FULFILLMENT_ACCEPTED = 3;
-export const FULFILLMENT_ACCEPTED_FULFILLER = 4;
-// export const BOUNTY_EXPIRED = 5
-export const BOUNTY_ISSUED = 6;
-export const BOUNTY_KILLED = 7;
-export const CONTRIBUTION_ADDED = 8;
-export const DEADLINE_EXTENDED = 9;
-export const BOUNTY_CHANGED = 10;
-export const ISSUER_TRANSFERRED = 11;
-export const TRANSFER_RECIPIENT = 12;
-export const PAYOUT_INCREASED = 13;
-export const BOUNTY_EXPIRED = 14;
-// ^^^ using this one for now since that is how the api is coding them
-// definitely need to fix this at some point :-)
-export const BOUNTY_COMMENT = 15;
-export const BOUNTY_ISSUED_ACTIVATED = 16;
-export const FULFILLMENT_UPDATED = 17;
-export const FULFILLMENT_UPDATED_ISSUER = 18;
-export const RATING_ISSUED = 19;
-export const RATING_RECEIVED = 20;
-export const PROFILE_UPDATED = 21;
-
-export const NOTIFICATION_TO_ID = {
-  FulfillmentSubmitted: FULFILLMENT_SUBMITTED,
-  FulfillmentSubmittedIssuer: FULFILLMENT_SUBMITTED_ISSUER,
-  BountyActivated: BOUNTY_ACTIVATED,
-  FulfillmentAccepted: FULFILLMENT_ACCEPTED,
-  FulfillmentAcceptedFulfiller: FULFILLMENT_ACCEPTED_FULFILLER,
-  BountyExpired: BOUNTY_EXPIRED,
-  BountyIssued: BOUNTY_ISSUED,
-  BountyKilled: BOUNTY_KILLED,
-  ContributionAdded: CONTRIBUTION_ADDED,
-  DeadlineExtended: DEADLINE_EXTENDED,
-  BountyChanged: BOUNTY_CHANGED,
-  IssuerTransferred: ISSUER_TRANSFERRED,
-  TransferRecipient: TRANSFER_RECIPIENT,
-  PayoutIncreased: PAYOUT_INCREASED,
-  BountyComment: BOUNTY_COMMENT,
-  BountyIssuedActivated: BOUNTY_ISSUED_ACTIVATED,
-  FulfillmentUpdated: FULFILLMENT_UPDATED,
-  FulfillmentUpdatedIssuer: FULFILLMENT_UPDATED_ISSUER,
-  RatingIssued: RATING_ISSUED,
-  RatingReceived: RATING_RECEIVED,
-  ProfileUpdated: PROFILE_UPDATED
+export const NOTIFICATION_ID = {
+  FULFILLMENT_SUBMITTED: 0,
+  FULFILLMENT_SUBMITTED_ISSUER: 1,
+  BOUNTY_ACTIVATED: 2,
+  FULFILLMENT_ACCEPTED: 3,
+  FULFILLMENT_ACCEPTED_FULFILLER: 4,
+  // BOUNTY_EXPIRED: 5
+  BOUNTY_ISSUED: 6,
+  BOUNTY_KILLED: 7,
+  CONTRIBUTION_ADDED: 8,
+  DEADLINE_EXTENDED: 9,
+  BOUNTY_CHANGED: 10,
+  ISSUER_TRANSFERRED: 11,
+  TRANSFER_RECIPIENT: 12,
+  PAYOUT_INCREASED: 13,
+  BOUNTY_EXPIRED: 14,
+  // ^^^ using this one for now since that is how the api is coding them
+  // definitely need to fix this at some point :-)
+  BOUNTY_COMMENT: 15,
+  BOUNTY_ISSUED_ACTIVATED: 16,
+  FULFILLMENT_UPDATED: 17,
+  FULFILLMENT_UPDATED_ISSUER: 18,
+  RATING_ISSUED: 19,
+  RATING_RECEIVED: 20,
+  PROFILE_UPDATED: 21
 };
-
-export const ID_TO_NOTIFICATION = invert(NOTIFICATION_TO_ID);
