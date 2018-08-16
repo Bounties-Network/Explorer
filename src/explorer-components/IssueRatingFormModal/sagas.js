2de3455ba1ece4384e2d5c69c88c304e25b44436
Matt Garnett <matt@mattgarnett.com>
2018-08-16 10:04:56 -0500

    ratings modal only depends on fulfillmentId now

---
+++ src/explorer-components/IssueRatingFormModal/sagas.js
@@ -0,0 +1,68 @@
+import request from 'utils/request';
+import { call, put, select, takeLatest } from 'redux-saga/effects';
+import { actionTypes, actions } from './reducer';
+import {
+  fulfillmentsSelector,
+  fulfillmentsQuerySelector
+} from 'public-modules/Fulfillments/selectors';
+
+const { LOAD_REVIEWEE } = actionTypes;
+const { loadRevieweeSuccess, loadRevieweeFail } = actions;
+
+export function* loadReviewee(action) {
+  const { identifiers } = action;
+  const { bountyId, fulfillmentId, type } = identifiers;
+
+  try {
+    let endpoint = `fulfillment/?bounty=${bountyId}&fulfillment_id=${fulfillmentId}`;
+    const fulfillments = yield call(request, endpoint, 'GET');
+    const fulfillment = fulfillments.results[0];
+
+    let reviewee = {
+      name: fulfillment.user.name,
+      address: fulfillment.user.public_address,
+      img: fulfillment.user.profile_image
+    };
+
+    if (type === 'issuer') {
+      const { bounty_data } = fulfillment;
+
+      reviewee = {
+        name: bounty_data.user.name,
+        address: bounty_data.user.public_address,
+        img: bounty_data.user.profile_image
+      };
+    }
+
+    yield put(loadRevieweeSuccess(reviewee));
+  } catch (e) {
+    console.log(e);
+    yield put(loadRevieweeFail(e));
+  }
+}
+
+// export function* loadMoreFulfillments() {
+//   const params = yield select(fulfillmentsQuerySelector);
+//   const offset = (yield select(fulfillmentsSelector)).fulfillments.length;
+//
+//   params['offset'] = offset;
+//
+//   try {
+//     let endpoint = 'fulfillment';
+//     const fulfillments = yield call(request, endpoint, 'GET', { params });
+//     const { results, count } = fulfillments;
+//     yield put(loadMoreFulfillmentsSuccess(results, count));
+//   } catch (e) {
+//     yield put(loadMoreFulfillmentsFail(e));
+//   }
+// }
+
+export function* watchReviewee() {
+  yield takeLatest(LOAD_REVIEWEE, loadReviewee);
+}
+
+// export function* watchLoadMoreFulfillments() {
+//   yield takeLatest(LOAD_MORE_FULFILLMENTS, loadMoreFulfillments);
+// }
+
+export default [watchReviewee];
