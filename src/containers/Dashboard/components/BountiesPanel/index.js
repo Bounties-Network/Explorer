642e78f063e4c508b17357ff0eb2145f7ba9b61a
mbeylin <mark_beylin@hotmail.com>
2018-08-19 14:12:18 -0400

    switched out meh icon for content-specific zero state icons

--- src/containers/Dashboard/components/BountiesPanel/index.js
+++ src/containers/Dashboard/components/BountiesPanel/index.js
@@ -115,7 +115,7 @@ class BountiesPanelComponent extends React.Component {
             actionText={'Create new bounty'}
             onActionClick={() => history.push('/createBounty')}
             iconColor="blue"
-            icon={['far', 'plus']}
+            icon={['far', 'plus-circle']}
           />
         </div>
       );
