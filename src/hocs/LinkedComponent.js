09714f59fd44386e62006a259494e6eae95585e9
William Villanueva <wvillanu89@gmail.com>
2018-08-30 22:46:00 -0600

    Scroll to top.

--- src/hocs/LinkedComponent.js
+++ src/hocs/LinkedComponent.js
@@ -3,11 +3,12 @@ import { withRouter } from 'react-router-dom';
 
 function LinkedComponentHOC(WrappedComponent) {
   const LinkedComponent = props => {
-    const { history, to } = props;
+    const { history, to, onClick } = props;
 
     const handler = e => {
       e.preventDefault();
       history.push(to);
+      onClick(e);
     };
 
     return <WrappedComponent {...props} onClick={handler} />;
