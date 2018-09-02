a1cc664207074511028da82a90357c2f799e4ecd
William Villanueva <wvillanu89@gmail.com>
2018-09-01 20:19:48 -0500

    Fix modal bug when the modal is the unfixed version.

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -170,7 +170,16 @@ class Modal extends React.Component {
   };
 
   componentWillUnmount() {
+    const { fixed } = this.props;
+
     const currentModals = document.getElementsByClassName('modal-visible');
+    if (!fixed) {
+      const pageBody = document.getElementsByClassName('page-body')[0];
+      if (pageBody) {
+        pageBody.classList.remove('modal-open');
+      }
+      document.body.classList.remove('modal-open');
+    }
     if (currentModals && currentModals.length) {
       return null;
     }
