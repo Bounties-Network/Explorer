2a07f6b2d398ed051923851ac503c0b239d8c5e5
Matt Garnett <matt@mattgarnett.com>
2018-09-23 10:17:35 -0500

    add loader to text inputs

--- src/hocs/FormInput.js
+++ src/hocs/FormInput.js
@@ -3,14 +3,21 @@ import React from 'react';
 function FormInputHOC(WrappedComponent) {
   return props => {
     const {
-      meta: { touched, error },
+      meta: { touched, error, asyncValidating },
       input
     } = props;
     let componentError = '';
     if (error && touched) {
       componentError = error;
     }
-    return <WrappedComponent {...props} {...input} error={componentError} />;
+    return (
+      <WrappedComponent
+        {...props}
+        {...input}
+        error={componentError}
+        loading={asyncValidating}
+      />
+    );
   };
 }
 
