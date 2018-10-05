10a1742662d3d5b0157f82ea5021c3340d51c12d
Matt Garnett <matt@mattgarnett.com>
2018-10-04 21:14:21 -0500

    add utility function to expand platforms into their subplatforms

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,6 @@
+import config from 'public-modules/config';
+import { flatten, map } from 'lodash';
+
 export function getTimezone() {
   if (Intl && Intl.DateTimeFormat) {
     const dateTimeFormat = Intl.DateTimeFormat();
@@ -110,3 +113,14 @@ export function promisifyDebounce(inner, ms = 0) {
     return new Promise(r => resolves.push(r));
   };
 }
+
+export const expandPlatforms = platforms => {
+  if (!config.subPlatforms) return platforms;
+
+  return flatten(
+    map(platform => {
+      const subPlatforms = config.subPlatforms[platform];
+      return subPlatforms ? [platform, ...subPlatforms] : platform;
+    }, platforms)
+  );
+};
