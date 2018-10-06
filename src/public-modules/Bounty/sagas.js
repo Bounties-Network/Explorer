b82ac56793548f39913da041753a2cddea64265b
William Villanueva <wvillanu89@gmail.com>
2018-10-05 23:13:34 -0700

    Update to backup contract for DAI

--- src/public-modules/Bounty/sagas.js
+++ src/public-modules/Bounty/sagas.js
@@ -53,12 +53,23 @@ const {
 } = transactionActions;
 
 export function* getTokenData(tokenAddress) {
-  const { tokenContract: tokenContractClient } = yield call(
-    getTokenClient,
-    tokenAddress
-  );
-  const symbol = yield call(tokenContractClient.symbol().call);
-  const decimals = yield call(tokenContractClient.decimals().call);
+  let symbol, decimals;
+  try {
+    const { tokenContract: tokenContractClient } = yield call(
+      getTokenClient,
+      tokenAddress
+    );
+    symbol = yield call(tokenContractClient.symbol().call);
+    decimals = yield call(tokenContractClient.decimals().call);
+  } catch (e) {
+    const { tokenContract: tokenContractClient } = yield call(
+      getTokenClient,
+      tokenAddress,
+      'DSToken'
+    );
+    symbol = yield call(tokenContractClient.symbol().call);
+    decimals = yield call(tokenContractClient.decimals().call);
+  }
   return {
     symbol,
     decimals
