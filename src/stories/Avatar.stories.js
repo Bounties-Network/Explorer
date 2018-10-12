149fda4baa5d14daa713c087dbc90a697ee831f2
I'm Luis! ^-^ <malbertoa_11@hotmail.com>
2018-10-12 15:50:59 -0400

    Add documentation for the Avatar component

--- src/stories/Avatar.stories.js
+++ src/stories/Avatar.stories.js
@@ -6,76 +6,395 @@ import { action } from '@storybook/addon-actions';
 import { Avatar, Text } from 'components';
 
 storiesOf('Avatar', module).add('Avatar', () => (
-  <div>
-    <Text type="H3">Avatar</Text>
-    <div>
-      <div style={{ marginTop: '10px' }} />
-      <Text type="H4">With border, name and address (default text)</Text>
-      <span style={{ marginRight: '10px' }} />
-      <Avatar
-        size="large"
-        border
-        name="Simona Pop"
-        img="https://i.imgur.com/lhTwRZY.png"
-        address="0xe68f8C6AB137ecDGD5cbf131f74A584aD2fG294r"
-      />
-      <div style={{ marginTop: '10px' }} />
-      <Text type="H4">With border, name and address (changed text styles)</Text>
-      <span style={{ marginRight: '10px' }} />
-      <div
-        style={{
-          width: '300px',
-          height: '150px',
-          backgroundColor: '#5A28C6',
-          paddingTop: '20px'
-        }}
-      >
-        <Avatar
-          size="large"
-          border
-          name="Simona Pop"
-          address="0xe68f8C6AB137ecDGD5cbf131f74A584aD2fG294r"
-          nameTextColor="white"
-          nameTextType="H2"
-          img="https://i.imgur.com/lhTwRZY.png"
-          addressTextColor="white"
-        />
-      </div>
-      <div style={{ marginTop: '10px' }} />
-      <Text type="H4">
-        No border, no name and with an address (changed text styles)
-      </Text>
-      <span style={{ marginRight: '10px' }} />
-      <div
-        style={{
-          width: '300px',
-          height: '150px',
-          backgroundColor: '#5A28C6',
-          paddingTop: '20px'
-        }}
-      >
-        <Avatar
-          size="small"
-          address="0xe68f8C6AB137ecDGD5cbf131f74A584aD2fG294r"
-          img="https://i.imgur.com/lhTwRZY.png"
-          addressTextColor="white"
-          addressTextType="H4"
-        />
-      </div>
-      <div style={{ marginTop: '10px' }} />
-      <Text type="H4">No name or address</Text>
-      <span style={{ marginRight: '10px' }} />
+  <div class="sb-page-wrapper">
+    <Text
+      className={'sb-component-group-heading'}
+      typeScale="h1"
+      color="purple"
+      weight="fontWeight-bold"
+    >
+      Avatar
+    </Text>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      Avatar components take props to display beautiful avatars. You can
+      customize the avatar and surrounding information such as name and address.
+    </Text>
+
+    <Text
+      className={'sb-component-group-subheading'}
+      typeScale="h3"
+      weight="fontWeight-bold"
+    >
+      Image
+    </Text>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      The <code>img</code> prop will determine the image to be displayed.
+    </Text>
+
+    <div class="sb-component-group sb-button-group">
       <Avatar size="large" img="https://i.imgur.com/lhTwRZY.png" />
-      <div style={{ marginTop: '10px' }} />
-      <Text type="H4">Small</Text>
-      <span style={{ marginRight: '10px' }} />
-      <Avatar size="small" img="https://i.imgur.com/lhTwRZY.png" />
-      <div style={{ marginTop: '10px' }} />
-      <Text type="H4">Large</Text>
-      <span style={{ marginRight: '10px' }} />
-      <Avatar size="large" img="https://i.imgur.com/lhTwRZY.png" />
-      <Text type="H4">No Image</Text>
+    </div>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      The <code>hash</code> prop will create an image based on the hash.
+    </Text>
+
+    <div class="sb-component-group sb-button-group">
       <Avatar size="large" hash="0xe68f8C6AB137ecDGD5cbf131f74A584aD2fG294r" />
     </div>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      The <code>size</code> prop will determine the size of the image. It can be{' '}
+      <code>small</code>, <code>medium</code> or <code>large</code>. The default
+      is <code>small</code>.
+    </Text>
+
+    <div class="sb-component-group sb-button-group">
+      <Avatar size="small" img="https://i.imgur.com/lhTwRZY.png" />
+      <br />
+      <Avatar size="medium" img="https://i.imgur.com/lhTwRZY.png" />
+      <br />
+      <Avatar size="large" img="https://i.imgur.com/lhTwRZY.png" />
+    </div>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      The <code>border</code> prop is a boolean that will determine whether the
+      image will have a border or not. The default is <code>false</code>.
+    </Text>
+
+    <div
+      class="sb-component-group sb-button-group"
+      style={{ backgroundColor: '#EEE' }}
+    >
+      <Avatar
+        size="medium"
+        img="https://i.imgur.com/lhTwRZY.png"
+        border={true}
+      />
+      <br />
+      <Avatar
+        size="medium"
+        img="https://i.imgur.com/lhTwRZY.png"
+        border={false}
+      />
+    </div>
+
+    <Text
+      className={'sb-component-group-subheading'}
+      typeScale="h3"
+      weight="fontWeight-bold"
+    >
+      Name
+    </Text>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      The <code>name</code> prop will determine the name of the person
+      represented in the avatar.
+    </Text>
+
+    <div class="sb-component-group sb-button-group">
+      <Avatar
+        size="large"
+        img="https://i.imgur.com/lhTwRZY.png"
+        name="Simona Pop"
+      />
+    </div>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      The <code>nameTextScale</code> prop will determine the size of the name.
+      It can go from <code>h1</code> to <code>h6</code> or be <code>Body</code>{' '}
+      or <code>Small</code>. The default is <code>h3</code>.
+    </Text>
+
+    <div class="sb-component-group sb-button-group">
+      <Avatar
+        size="small"
+        img="https://i.imgur.com/lhTwRZY.png"
+        name="Simona Pop"
+        nameTextScale="Small"
+      />
+      <br />
+      <Avatar
+        size="medium"
+        img="https://i.imgur.com/lhTwRZY.png"
+        name="Simona Pop"
+        nameTextScale="h3"
+      />
+      <br />
+      <Avatar
+        size="large"
+        img="https://i.imgur.com/lhTwRZY.png"
+        name="Simona Pop"
+        nameTextScale="h1"
+      />
+    </div>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      The <code>nameTextColor</code> prop will determine the color of the name.
+      It can be <code>purple</code>, <code>blue</code>, <code>orange</code>,{' '}
+      <code>green</code>, <code>red</code>, <code>black</code>,{' '}
+      <code>white</code>, <code>defaultGrey</code>, <code>lightGrey</code> or{' '}
+      <code>darkGrey</code>.
+    </Text>
+
+    <div class="sb-component-group sb-button-group">
+      <Avatar
+        size="small"
+        img="https://i.imgur.com/lhTwRZY.png"
+        name="Simona Pop"
+        nameTextScale="Small"
+        nameTextColor="purple"
+      />
+      <br />
+      <Avatar
+        size="medium"
+        img="https://i.imgur.com/lhTwRZY.png"
+        name="Simona Pop"
+        nameTextScale="h3"
+        nameTextColor="green"
+      />
+      <br />
+      <Avatar
+        size="large"
+        img="https://i.imgur.com/lhTwRZY.png"
+        name="Simona Pop"
+        nameTextScale="h1"
+        nameTextColor="red"
+      />
+    </div>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      The <code>nameTextWeight</code> prop will determine the weight of the text
+      in the name. It can be <code>fontWeight-regular</code>,{' '}
+      <code>fontWeight-medium</code> or <code>fontWeight-bold</code>. The
+      default is <code>fontWeight-bold</code>.
+    </Text>
+
+    <div class="sb-component-group sb-button-group">
+      <Avatar
+        size="large"
+        img="https://i.imgur.com/lhTwRZY.png"
+        name="Simona Pop"
+        nameTextWeight="fontWeight-regular"
+      />
+      <br />
+      <Avatar
+        size="large"
+        img="https://i.imgur.com/lhTwRZY.png"
+        name="Simona Pop"
+        nameTextWeight="fontWeight-medium"
+      />
+      <br />
+      <Avatar
+        size="large"
+        img="https://i.imgur.com/lhTwRZY.png"
+        name="Simona Pop"
+      />
+    </div>
+
+    <Text
+      className={'sb-component-group-subheading'}
+      typeScale="h3"
+      weight="fontWeight-bold"
+    >
+      Address
+    </Text>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      The <code>address</code> prop will determine the address of the person
+      represented in the avatar.
+    </Text>
+
+    <div class="sb-component-group sb-button-group">
+      <Avatar
+        size="large"
+        img="https://i.imgur.com/lhTwRZY.png"
+        name="Simona Pop"
+        address="0xe68f8C6AB137ecDGD5cbf131f74A584aD2fG294r"
+      />
+    </div>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      The <code>addressTextScale</code> prop will determine the size of the
+      address. It can go from <code>h1</code> to <code>h6</code> or be{' '}
+      <code>Body</code> or <code>Small</code>. The default is <code>Body</code>.
+    </Text>
+
+    <div class="sb-component-group sb-button-group">
+      <Avatar
+        size="small"
+        img="https://i.imgur.com/lhTwRZY.png"
+        name="Simona Pop"
+        nameTextScale="Small"
+        address="0xe68f8C6AB137ecDGD5cbf131f74A584aD2fG294r"
+        addressTextScale="Small"
+      />
+      <br />
+      <Avatar
+        size="medium"
+        img="https://i.imgur.com/lhTwRZY.png"
+        name="Simona Pop"
+        nameTextScale="h3"
+        address="0xe68f8C6AB137ecDGD5cbf131f74A584aD2fG294r"
+        addressTextScale="h5"
+      />
+      <br />
+      <Avatar
+        size="large"
+        img="https://i.imgur.com/lhTwRZY.png"
+        name="Simona Pop"
+        nameTextScale="h1"
+        address="0xe68f8C6AB137ecDGD5cbf131f74A584aD2fG294r"
+        addressTextScale="h3"
+      />
+    </div>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      The <code>addressTextColor</code> prop will determine the color of the
+      address. It can be <code>purple</code>, <code>blue</code>,{' '}
+      <code>orange</code>, <code>green</code>, <code>red</code>,{' '}
+      <code>black</code>, <code>white</code>, <code>defaultGrey</code>,{' '}
+      <code>lightGrey</code> or <code>darkGrey</code>.
+    </Text>
+
+    <div class="sb-component-group sb-button-group">
+      <Avatar
+        size="large"
+        img="https://i.imgur.com/lhTwRZY.png"
+        name="Simona Pop"
+        address="0xe68f8C6AB137ecDGD5cbf131f74A584aD2fG294r"
+        addressTextColor="red"
+      />
+      <br />
+      <Avatar
+        size="large"
+        img="https://i.imgur.com/lhTwRZY.png"
+        name="Simona Pop"
+        address="0xe68f8C6AB137ecDGD5cbf131f74A584aD2fG294r"
+        addressTextColor="green"
+      />
+      <br />
+      <Avatar
+        size="large"
+        img="https://i.imgur.com/lhTwRZY.png"
+        name="Simona Pop"
+        address="0xe68f8C6AB137ecDGD5cbf131f74A584aD2fG294r"
+        addressTextColor="blue"
+      />
+    </div>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      The <code>src</code> prop will determine the link of the address. If not
+      passed, it will link to <code>{'profile/{address}'}</code>.
+    </Text>
+
+    <div class="sb-component-group sb-button-group">
+      <Avatar
+        size="large"
+        img="https://i.imgur.com/lhTwRZY.png"
+        name="Simona Pop"
+        address="0xe68f8C6AB137ecDGD5cbf131f74A584aD2fG294r"
+        src="https://www.google.com/"
+      />
+      <br />
+      <Avatar
+        size="large"
+        img="https://i.imgur.com/lhTwRZY.png"
+        name="Simona Pop"
+        address="0xe68f8C6AB137ecDGD5cbf131f74A584aD2fG294r"
+      />
+    </div>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      The <code>onClick</code> prop is a function that will be executed when the
+      address is clicked.
+    </Text>
+
+    <div class="sb-component-group sb-button-group">
+      <Avatar
+        size="large"
+        img="https://i.imgur.com/lhTwRZY.png"
+        name="Simona Pop"
+        address="0xe68f8C6AB137ecDGD5cbf131f74A584aD2fG294r"
+        onClick={() => {
+          alert('You clicked me!');
+        }}
+      />
+    </div>
+
+    <Text
+      className={'sb-component-group-subheading'}
+      typeScale="h3"
+      weight="fontWeight-bold"
+    >
+      Misc
+    </Text>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      The <code>className</code> prop is a string that contains classes to be
+      added to the root div of the avatar.
+    </Text>
   </div>
 ));
