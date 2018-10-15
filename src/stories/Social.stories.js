63f0ad3d82d8b98cf9a7ed95ebfcd397bb05313c
I'm Luis! ^-^ <malbertoa_11@hotmail.com>
2018-10-15 19:44:27 -0400

    Add documentation for the Social component

--- src/stories/Social.stories.js
+++ src/stories/Social.stories.js
@@ -6,14 +6,44 @@ import { action } from '@storybook/addon-actions';
 import { Social, Text } from 'components';
 
 storiesOf('Social', module).add('Social', () => (
-  <div style={{ margin: '10px' }}>
-    <div style={{ marginTop: '10px' }} />
-    <Text type="BodySmall">Social Component</Text>
-    <div style={{ marginTop: '10px' }} />
-    <Social
-      facebookLink="https://facebook.com"
-      twitterLink="https://twitter.com"
-      redditLink="https://reddit.com"
-    />
+  <div class="sb-page-wrapper">
+    <Text
+      className={'sb-component-group-heading'}
+      typeScale="h1"
+      color="purple"
+      weight="fontWeight-bold"
+    >
+      Social
+    </Text>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      Social components create links to share the current URL to different
+      social media sites.
+    </Text>
+
+    <Text
+      className={'sb-component-group-subheading'}
+      typeScale="h3"
+      weight="fontWeight-bold"
+    >
+      Regular Social component
+    </Text>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      A regular Social component takes no props and renders 3 links to share the
+      current page URL.
+    </Text>
+
+    <div class="sb-component-group sb-button-group">
+      <Social />
+    </div>
   </div>
 ));
