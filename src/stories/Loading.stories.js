e1c47e670d337fc1d47edb5a3942707a881be301
I'm Luis! ^-^ <malbertoa_11@hotmail.com>
2018-10-14 21:48:21 -0400

    Add documentation for the Loading component

--- src/stories/Loading.stories.js
+++ src/stories/Loading.stories.js
@@ -2,10 +2,29 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 
-import { Loading } from 'components';
+import { Loading, Text } from 'components';
 
 storiesOf('Loading', module).add('Loading', () => (
-  <div>
-    <Loading />
+  <div class="sb-page-wrapper">
+    <Text
+      className={'sb-component-group-heading'}
+      typeScale="h1"
+      color="purple"
+      weight="fontWeight-bold"
+    >
+      Loading
+    </Text>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      Loading components will render a simple loading animation.
+    </Text>
+
+    <div class="sb-component-group sb-button-group">
+      <Loading />
+    </div>
   </div>
 ));
