a3eb52aaf3f493c98b64f44a092eab59a26b2657
I'm Luis! ^-^ <malbertoa_11@hotmail.com>
2018-10-15 14:21:11 -0400

    Add documentation for the NumberInput component

--- src/stories/NumberInput.stories.js
+++ src/stories/NumberInput.stories.js
@@ -6,18 +6,170 @@ import { action } from '@storybook/addon-actions';
 import { NumberInput, Text } from 'components';
 
 storiesOf('NumberInput', module).add('NumberInput', () => (
-  <div style={{ margin: '10px' }}>
-    <div style={{ marginTop: '10px' }} />
-    <Text type="BodySmall">Regular</Text>
-    <div style={{ marginTop: '10px' }} />
-    <NumberInput />
-    <div style={{ marginTop: '10px' }} />
-    <Text type="BodySmall">Disabled</Text>
-    <div style={{ marginTop: '10px' }} />
-    <NumberInput disabled />
-    <div style={{ marginTop: '10px' }} />
-    <Text type="BodySmall">Label</Text>
-    <div style={{ marginTop: '10px' }} />
-    <NumberInput label="Revisions" />
+  <div class="sb-page-wrapper">
+    <Text
+      className={'sb-component-group-heading'}
+      typeScale="h1"
+      color="purple"
+      weight="fontWeight-bold"
+    >
+      NumberInput
+    </Text>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      NumberInput components can be used to select number within a range.
+    </Text>
+
+    <Text
+      className={'sb-component-group-subheading'}
+      typeScale="h3"
+      weight="fontWeight-bold"
+    >
+      defaultValue
+    </Text>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      The <code>defaultValue</code> prop will determine the default value of the
+      input. This may be changed later by the user. The default value is{' '}
+      <code>0</code>.
+    </Text>
+
+    <div class="sb-component-group sb-button-group">
+      <NumberInput defaultValue={5} />
+    </div>
+
+    <Text
+      className={'sb-component-group-subheading'}
+      typeScale="h3"
+      weight="fontWeight-bold"
+    >
+      value
+    </Text>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      The <code>value</code> prop will determine the value of the input. This
+      can only be changed programatically.
+    </Text>
+
+    <div class="sb-component-group sb-button-group">
+      <NumberInput value={5} />
+    </div>
+
+    <Text
+      className={'sb-component-group-subheading'}
+      typeScale="h3"
+      weight="fontWeight-bold"
+    >
+      label
+    </Text>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      The <code>label</code> prop will determine the label of the input.
+    </Text>
+
+    <div class="sb-component-group sb-button-group">
+      <NumberInput value={5} label="I am the label" />
+    </div>
+
+    <Text
+      className={'sb-component-group-subheading'}
+      typeScale="h3"
+      weight="fontWeight-bold"
+    >
+      disabled
+    </Text>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      The <code>disabled</code> prop will determine whether the user can change
+      the state of the input or not.
+    </Text>
+
+    <div class="sb-component-group sb-button-group">
+      <NumberInput value={5} disabled />
+    </div>
+
+    <Text
+      className={'sb-component-group-subheading'}
+      typeScale="h3"
+      weight="fontWeight-bold"
+    >
+      min
+    </Text>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      The <code>min</code> prop will determine the minimum value that the input
+      can accept. The default value is <code>0</code>.
+    </Text>
+
+    <div class="sb-component-group sb-button-group">
+      <NumberInput defaultValue={5} min={3} />
+    </div>
+
+    <Text
+      className={'sb-component-group-subheading'}
+      typeScale="h3"
+      weight="fontWeight-bold"
+    >
+      max
+    </Text>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      The <code>max</code> prop will determine the maximum value that the input
+      can accept. The default value is <code>10</code>.
+    </Text>
+
+    <div class="sb-component-group sb-button-group">
+      <NumberInput defaultValue={5} max={7} />
+    </div>
+
+    <Text
+      className={'sb-component-group-subheading'}
+      typeScale="h3"
+      weight="fontWeight-bold"
+    >
+      onChange
+    </Text>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      The <code>onChange</code> prop is a function that will be fired when the
+      user increases or decreases the value of the input. The new value of the
+      input will be sent as an argument.
+    </Text>
+
+    <div class="sb-component-group sb-button-group">
+      <NumberInput defaultValue={5} onChange={action('changed')} />
+    </div>
   </div>
 ));
