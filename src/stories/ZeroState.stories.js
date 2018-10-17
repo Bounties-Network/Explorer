1023a5c7ea6335b6645f24c0c7850ebe7d4e6779
I'm Luis! ^-^ <malbertoa_11@hotmail.com>
2018-10-16 23:24:03 -0400

    Add documentation for the ZeroState component

--- src/stories/ZeroState.stories.js
+++ src/stories/ZeroState.stories.js
@@ -5,74 +5,234 @@ import { action } from '@storybook/addon-actions';
 
 import { ZeroState, Text, Card } from 'components';
 
-storiesOf('ZeroState', module).add('ZeroState', () => (
-  <div style={{ margin: '10px', width: '500px' }}>
-    <div style={{ marginTop: '10px' }} />
-    <Text type="BodySmall">Zero State No Action</Text>
-    <div style={{ marginTop: '10px' }} />
-    <Card>
-      <Card.Body>
-        <div style={{ padding: '20px 50px' }}>
-          <ZeroState
-            title={'You have 0 active bounties'}
-            text={
-              "It looks like you don't have any active bounties at the moment. Enter a title for a new bounty here to get started creating one!"
-            }
-          />
-        </div>
-      </Card.Body>
-    </Card>
-    <div style={{ marginTop: '10px' }} />
-    <Text type="BodySmall">Zero State Action</Text>
-    <div style={{ marginTop: '10px' }} />
-    <Card>
-      <Card.Body>
-        <div style={{ padding: '20px 50px' }}>
-          <ZeroState
-            title={'You have 0 active bounties'}
-            text={
-              "It looks like you don't have any active bounties at the moment. Enter a title for a new bounty here to get started creating one!"
-            }
-            action
-            actionText={'Create new bounty'}
-          />
-        </div>
-      </Card.Body>
-    </Card>
-    <div style={{ marginTop: '10px' }} />
-    <Text type="BodySmall">Custom Icon</Text>
-    <div style={{ marginTop: '10px' }} />
-    <Card>
-      <Card.Body>
-        <div style={{ padding: '20px 50px' }}>
-          <ZeroState
-            title={'You have 0 active bounties'}
-            text={
-              "It looks like you don't have any active bounties at the moment. Enter a title for a new bounty here to get started creating one!"
-            }
-            action
-            actionText={'Create new bounty'}
-            icon={['fal', 'trophy-alt']}
-          />
-        </div>
-      </Card.Body>
-    </Card>
-    <div style={{ marginTop: '10px' }} />
-    <Text type="BodySmall">Error Type</Text>
-    <div style={{ marginTop: '10px' }} />
-    <Card>
-      <Card.Body>
-        <div style={{ padding: '20px 50px' }}>
-          <ZeroState
-            type="error"
-            title={'You have 0 active bounties'}
-            text={
-              "It looks like you don't have any active bounties at the moment. Enter a title for a new bounty here to get started creating one!"
-            }
-            icon={['fal', 'exclamation-triangle']}
-          />
-        </div>
-      </Card.Body>
-    </Card>
+storiesOf('Card', module).add('ZeroState', () => (
+  <div class="sb-page-wrapper">
+    <Text
+      className={'sb-component-group-heading'}
+      typeScale="h1"
+      color="purple"
+      weight="fontWeight-bold"
+    >
+      ZeroState
+    </Text>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      ZeroState components are useful to let the user know that something is
+      empty.
+    </Text>
+
+    <Text
+      className={'sb-component-group-subheading'}
+      typeScale="h3"
+      weight="fontWeight-bold"
+    >
+      Regular ZeroState
+    </Text>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      A ZeroState component goes within the <code>Card.Body</code> of a Card
+      component. It must contain the props <code>title</code> and{' '}
+      <code>text</code>.
+    </Text>
+
+    <div class="sb-component-group sb-button-group">
+      <Card>
+        <Card.Body>
+          <div style={{ padding: '20px 50px' }}>
+            <ZeroState
+              title={'You have 0 active bounties'}
+              text={
+                "It looks like you don't have any active bounties at the moment. Enter a title for a new bounty here to get started creating one!"
+              }
+            />
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
+
+    <Text
+      className={'sb-component-group-subheading'}
+      typeScale="h3"
+      weight="fontWeight-bold"
+    >
+      type
+    </Text>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      The <code>type</code> prop will determine the type of the ZeroState. It
+      can be <code>zero</code> or <code>error</code>. The default value is{' '}
+      <code>zero</code>.
+    </Text>
+
+    <div class="sb-component-group sb-button-group">
+      <Card>
+        <Card.Body>
+          <div style={{ padding: '20px 50px' }}>
+            <ZeroState
+              title={'You have 0 active bounties'}
+              text={
+                "It looks like you don't have any active bounties at the moment. Enter a title for a new bounty here to get started creating one!"
+              }
+              type="error"
+            />
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
+
+    <Text
+      className={'sb-component-group-subheading'}
+      typeScale="h3"
+      weight="fontWeight-bold"
+    >
+      icon
+    </Text>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      The <code>icon</code> prop will determine the icon to be displayed. It
+      must be an array with a font awesome icon (e.g{' '}
+      <code>['['fal', 'trophy-alt']</code>).
+    </Text>
+
+    <div class="sb-component-group sb-button-group">
+      <Card>
+        <Card.Body>
+          <div style={{ padding: '20px 50px' }}>
+            <ZeroState
+              title={'You have 0 active bounties'}
+              text={
+                "It looks like you don't have any active bounties at the moment. Enter a title for a new bounty here to get started creating one!"
+              }
+              icon={['fal', 'trophy-alt']}
+            />
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
+
+    <Text
+      className={'sb-component-group-subheading'}
+      typeScale="h3"
+      weight="fontWeight-bold"
+    >
+      iconColor
+    </Text>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      The <code>iconColor</code> prop will determine the color of the icon to be
+      displayed. It must be an array with a font awesome icon (e.g{' '}
+      <code>['fal', 'trophy-alt']</code>).
+    </Text>
+
+    <div class="sb-component-group sb-button-group">
+      <Card>
+        <Card.Body>
+          <div style={{ padding: '20px 50px' }}>
+            <ZeroState
+              title={'You have 0 active bounties'}
+              text={
+                "It looks like you don't have any active bounties at the moment. Enter a title for a new bounty here to get started creating one!"
+              }
+              icon={['fal', 'trophy-alt']}
+              iconColor="blue"
+            />
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
+
+    <Text
+      className={'sb-component-group-subheading'}
+      typeScale="h3"
+      weight="fontWeight-bold"
+    >
+      action and actionText
+    </Text>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      The <code>action</code> prop will determine if the ZeroState will contain
+      an action. The default value is <code>false</code>. If this is true, then{' '}
+      <code>actionText</code> will be the action to be made.
+    </Text>
+
+    <div class="sb-component-group sb-button-group">
+      <Card>
+        <Card.Body>
+          <div style={{ padding: '20px 50px' }}>
+            <ZeroState
+              title={'You have 0 active bounties'}
+              text={
+                "It looks like you don't have any active bounties at the moment. Enter a title for a new bounty here to get started creating one!"
+              }
+              icon={['fal', 'trophy-alt']}
+              iconColor="blue"
+              action
+              actionText="Add a new bounty"
+            />
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
+
+    <Text
+      className={'sb-component-group-subheading'}
+      typeScale="h3"
+      weight="fontWeight-bold"
+    >
+      onActionClick
+    </Text>
+
+    <Text
+      className={'sb-component-group-description'}
+      typeScale="Body"
+      lineHeight="lineHeight-default"
+    >
+      The <code>onActionClick</code> prop is a function that will be fired
+      whenever the user clicks on the action.
+    </Text>
+
+    <div class="sb-component-group sb-button-group">
+      <Card>
+        <Card.Body>
+          <div style={{ padding: '20px 50px' }}>
+            <ZeroState
+              title={'You have 0 active bounties'}
+              text={
+                "It looks like you don't have any active bounties at the moment. Enter a title for a new bounty here to get started creating one!"
+              }
+              icon={['fal', 'trophy-alt']}
+              iconColor="blue"
+              action
+              actionText="Add a new bounty"
+              onActionClick={action('click')}
+            />
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
   </div>
 ));
