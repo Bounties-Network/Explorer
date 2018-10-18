c903e8db3cde4127d00cfd22f25bb230b1d85d05
Calvin Goodman <GOODMC4@nationwide.com>
2018-10-18 10:13:56 -0400

    Switched banner open state to be parent-managed

--- src/components/PageBanner/index.js
+++ src/components/PageBanner/index.js
@@ -2,53 +2,65 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './PageBanner.module.scss';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-import { isFunction } from 'lodash';
+import { Transition } from 'react-transition-group';
 
 const PageBanner = props => {
-  const { children, wrapClass, className, dismissable } = props;
+  const {
+    visible,
+    children,
+    wrapClass,
+    className,
+    dismissable,
+    onClose
+  } = props;
 
-  let elemRef = React.createRef();
-
-  const onClose = () => {
-    const node = elemRef.current;
+  const onExit = node => {
     const height = node.getBoundingClientRect().height;
 
     node.style.height = `${height}px`;
 
-    // allow time for height set to take effect for css transition to work properly.
+    // <Transition> component onExiting would be most appropriate for this but
+    // it does not happen in the next animation frame - meaning the css
+    // transition does not take effect and no animation of height happens.
     requestAnimationFrame(() => {
       node.style.height = '0px';
     });
-
-    if (isFunction(props.onClose)) props.onClose();
   };
 
   return (
-    <div
-      className={`${className} ${styles.pageBanner} ${
-        styles[dismissable ? 'pageBanner-dismissable' : '']
-      }`}
-      ref={elemRef}
+    <Transition
+      in={visible}
+      enter={false}
+      onExit={onExit}
+      unmountOnExit={true}
+      timeout={200}
     >
-      <div className={`${wrapClass}`}>
-        <div className={`${styles.pageBannerWrapper}`}>
-          <div className={`${styles.pageBannerContent}`}>{children}</div>
-          {dismissable && (
-            <div className={`${styles.pageBannerClose}`}>
-              <FontAwesomeIcon
-                icon={['fal', 'times']}
-                className={styles.closeIcon}
-                onClick={onClose}
-              />
-            </div>
-          )}
+      <div
+        className={`${className} ${styles.pageBanner} ${
+          styles[dismissable ? 'pageBanner-dismissable' : '']
+        }`}
+      >
+        <div className={`${wrapClass}`}>
+          <div className={`${styles.pageBannerWrapper}`}>
+            <div className={`${styles.pageBannerContent}`}>{children}</div>
+            {dismissable && (
+              <div className={`${styles.pageBannerClose}`}>
+                <FontAwesomeIcon
+                  icon={['fal', 'times']}
+                  className={styles.closeIcon}
+                  onClick={onClose}
+                />
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </Transition>
   );
 };
 
 PageBanner.propTypes = {
+  visible: PropTypes.bool,
   className: PropTypes.string,
   dismissable: PropTypes.bool,
   onClose: PropTypes.func,
@@ -56,8 +68,10 @@ PageBanner.propTypes = {
 };
 
 PageBanner.defaultProps = {
+  visible: true,
   dismissable: true,
-  wrapClass: 'pageWrapper'
+  wrapClass: 'pageWrapper',
+  onClose: () => {}
 };
 
 export default PageBanner;
