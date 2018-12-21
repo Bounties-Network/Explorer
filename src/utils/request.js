8cd587967ad6b9cc70c12c70c9bb25418d0e694e
William Villanueva <wvillanu89@gmail.com>
2018-12-21 15:57:58 -0700

    Update header name since inderscores are forbidden

--- src/utils/request.js
+++ src/utils/request.js
@@ -123,7 +123,9 @@ export default function(url, method, options, customErrorHandler) {
   const isCustomUrl = url.slice(0, 4) === 'http';
 
   if (!isCustomUrl) {
-    bakedOptions.headers['HAS_WALLET'] = !!(window.web3 || window.ethereum);
+    bakedOptions.headers['haswallet'] = !!(
+      window.web3 || window.ethereum
+    ).toString();
   }
 
   const requestUrl = isCustomUrl ? url : `${endpoint}/${url}`;
