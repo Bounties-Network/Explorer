53be0ee19e3654b7865960fc21f7077977c82b93
Matt Garnett <matt@mattgarnett.com>
2019-01-07 15:22:24 -0600

    add global saga to send new page view request only when pathname changes

--- src/layout/App/sagas.js
+++ src/layout/App/sagas.js
@@ -1,9 +1,24 @@
-import { take, takeLatest } from 'redux-saga/effects';
-import { actionTypes } from 'layout/App/reducer';
+import request from 'utils/request';
+import { take, takeLatest, select, put, call } from 'redux-saga/effects';
+import { actions, actionTypes } from 'layout/App/reducer';
 import { actionTypes as clientActionTypes } from 'public-modules/Client';
+import { lastLocationSelector } from './selectors';
+import { LOCATION_CHANGE } from 'react-router-redux';
 
+const { setLastLocation } = actions;
 const { GET_TOKEN_BALANCE } = actionTypes;
 const { GET_TOKEN_BALANCE_SUCCESS, GET_TOKEN_BALANCE_FAIL } = clientActionTypes;
+
+export function* issuePageView(action) {
+  const location = action.payload;
+  const lastLocation = yield select(lastLocationSelector);
+
+  if (lastLocation == null || location.pathname !== lastLocation.pathname) {
+    yield call(request, 'analytics/recordPageView/', 'GET');
+  }
+
+  yield put(setLastLocation(action.payload));
+}
 
 export function* getTokenBalance(action) {
   const { resolve, reject } = action;
@@ -25,4 +40,8 @@ export function* watchGetTokenBalance() {
   yield takeLatest(GET_TOKEN_BALANCE, getTokenBalance);
 }
 
-export default [watchGetTokenBalance];
+export function* watchLocationChange() {
+  yield takeLatest(LOCATION_CHANGE, issuePageView);
+}
+
+export default [watchGetTokenBalance, watchLocationChange];
--- src/layout/App/selectors.js
+++ src/layout/App/selectors.js
@@ -1,8 +1,14 @@
 import { createSelector } from 'reselect';
 
 export const rootAppSelector = state => state.app;
+export const routerSelector = state => state.router;
 
 export const locationNonceSelector = createSelector(
   rootAppSelector,
   rootApp => rootApp.locationNonce
+);
+
+export const lastLocationSelector = createSelector(
+  rootAppSelector,
+  rootApp => rootApp.lastLocation
 );
