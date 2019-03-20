2e73d894cf504cc6c8f451988311ed0b0bacd0fd
Matt Garnett <matt@mattgarnett.com>
2019-03-20 13:50:27 -0500

    updated infura endpoint to v3

--- src/public-modules/Utilities/Web3Client.js
+++ src/public-modules/Utilities/Web3Client.js
@@ -1,7 +1,9 @@
 import Web3 from 'web3';
 
 const web3 = new Web3(
-  new Web3.providers.HttpProvider('https://mainnet.infura.io')
+  new Web3.providers.HttpProvider(
+    'https://mainnet.infura.io/v3/5eb45628ce2c4ecebcce7f201f352792'
+  )
 );
 
 export default web3;
