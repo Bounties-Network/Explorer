3251c332fea4384b607028e508078cd2d416a1ed
Matt Garnett <14004106+c-o-l-o-r@users.noreply.github.com>
2019-03-31 11:35:22 -0300

    fixed increase payout modal

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -16,15 +16,19 @@ const minLength = min => value =>
   value && value.length < min ? `Must be ${min} characters or more` : undefined;
 
 const maxDecimals = max => value => {
-  const arr = value.split('.');
+  if (value) {
+    const arr = value.split('.');
 
-  if (arr.length < 2) {
-    return undefined;
+    if (arr.length < 2) {
+      return undefined;
+    }
+
+    return arr[1].length <= max
+      ? undefined
+      : `Must be ${max} decimal places or less`;
   }
 
-  return arr[1].length <= max
-    ? undefined
-    : `Must be ${max} decimal places or less`;
+  return undefined;
 };
 
 const minValue = min => value => {
