a09942d0e24ec8a2ff9da3ba5804ca228c7c5883
Eric Juta <ericjohn.juta@consensys.net>
2019-08-23 09:49:46 +0100

    Woo got prod buidl workin fine

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -114,33 +114,55 @@ module.exports = {
           {
             test: /\.(js|jsx|mjs)$/,
             include: paths.appSrc,
-            loader: require.resolve('babel-loader'),
-            options: {
-              compact: true
-            }
+            loader: require.resolve('babel-loader')
           },
-          // The notation here is somewhat confusing.
-          // "postcss" loader applies autoprefixer to our CSS.
-          // "css" loader resolves paths in CSS and adds assets as dependencies.
-          // "style" loader normally turns CSS into JS modules injecting <style>,
-          // but unlike in development configuration, we do something different.
-          // `ExtractTextPlugin` first applies the "postcss" and "css" loaders
-          // (second argument), then grabs the result CSS and puts it into a
-          // separate file in our build process. This way we actually ship
-          // a single CSS file in production instead of JS code injecting <style>
-          // tags. If you use code splitting, however, any async bundles will still
-          // use the "style" loader inside the async code so CSS from them won't be
-          // in the main CSS file.
           {
             test: /\.tsx?$/,
             use: 'ts-loader',
             exclude: /node_modules/
           },
           {
-            test: /\.(module\.sa|module\.sc|sa|sc|c)ss$/,
+            test: /\.module\.s(a|c)ss$/,
             use: [
               { loader: MiniCssExtractPlugin.loader },
-              { loader: 'css-loader', options: { minimize: true } },
+              {
+                loader: 'css-loader',
+                options: {
+                  modules: true
+                }
+              },
+              {
+                loader: 'postcss-loader',
+                options: {
+                  // Necessary for external CSS imports to work
+                  // https://github.com/facebookincubator/create-react-app/issues/2677
+                  ident: 'postcss',
+                  plugins: () => [
+                    require('postcss-flexbugs-fixes'),
+                    autoprefixer({
+                      browsers: [
+                        '>1%',
+                        'last 4 versions',
+                        'Firefox ESR',
+                        'not ie < 9' // React doesn't support IE8 anyway
+                      ],
+                      flexbox: 'no-2009'
+                    })
+                  ]
+                }
+              },
+              { loader: 'sass-loader' }
+            ]
+          },
+          // Global imports
+          {
+            test: /\.(c|sa|sc)ss$/,
+            exclude: /\.module.(s(a|c)ss)$/,
+            use: [
+              { loader: MiniCssExtractPlugin.loader },
+              {
+                loader: 'css-loader'
+              },
               {
                 loader: 'postcss-loader',
                 options: {
@@ -215,8 +237,6 @@ module.exports = {
     // Otherwise React will be compiled in the very slow development mode.
     new webpack.DefinePlugin(env.stringified),
     new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
       filename: cssFilename,
       chunkFilename: cssChunkFilename
     }),
@@ -226,36 +246,6 @@ module.exports = {
     new ManifestPlugin({
       fileName: 'asset-manifest.json'
     }),
-    // Generate a service worker script that will precache, and keep up to date,
-    // the HTML & assets that are part of the Webpack build.
-    // new SWPrecacheWebpackPlugin({
-    //   // By default, a cache-busting query parameter is appended to requests
-    //   // used to populate the caches, to ensure the responses are fresh.
-    //   // If a URL is already hashed by Webpack, then there is no concern
-    //   // about it being stale, and the cache-busting can be skipped.
-    //   dontCacheBustUrlsMatching: /\.\w{8}\./,
-    //   filename: 'service-worker.js',
-    //   logger(message) {
-    //     if (message.indexOf('Total precache size is') === 0) {
-    //       // This message occurs for every build and is a bit too noisy.
-    //       return;
-    //     }
-    //     if (message.indexOf('Skipping static resource') === 0) {
-    //       // This message obscures real errors so we ignore it.
-    //       // https://github.com/facebookincubator/create-react-app/issues/2612
-    //       return;
-    //     }
-    //     console.log(message);
-    //   },
-    //   minify: true,
-    //   // For unknown URLs, fallback to the index page
-    //   navigateFallback: publicUrl + '/index.html',
-    //   // Ignores URLs starting from /__ (useful for Firebase):
-    //   // https://github.com/facebookincubator/create-react-app/issues/2237#issuecomment-302693219
-    //   navigateFallbackWhitelist: [/^(?!\/__).*/],
-    //   // Don't precache sourcemaps (they're large) and build asset manifest:
-    //   staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
-    // }),
     // Moment.js is an extremely popular library that bundles large locale files
     // by default due to how Webpack interprets its code. This is a practical
     // solution that requires the user to opt into importing specific locales.
@@ -263,11 +253,11 @@ module.exports = {
     // You can remove this if you don't use Moment.js:
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
   ],
-  // Some libraries import Node modules but don't use them in the browser.
-  // Tell Webpack to provide empty mocks for them so importing them works.
   optimization: {
     minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
   },
+  // Some libraries import Node modules but don't use them in the browser.
+  // Tell Webpack to provide empty mocks for them so importing them works.
   node: {
     dgram: 'empty',
     fs: 'empty',
