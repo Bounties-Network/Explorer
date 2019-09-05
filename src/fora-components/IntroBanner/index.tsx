fcd4f2925d76e31b0d4081c57b0a5e0578327333
Eric Juta <ericjohn.juta@consensys.net>
2019-09-05 14:26:12 +0100

    Check in heading, description and secondary button variants

--- src/fora-components/IntroBanner/index.tsx
+++ src/fora-components/IntroBanner/index.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import { Text, Flex, Button } from 'rebass';
+import styled from 'lib/emotion-styled';
+import css from '@styled-system/css';
+
+const IntroBannerHeading = styled(Text)(props =>
+  css({
+    variant: 'text.introBannerH1'
+  })
+);
+
+const Container = styled(Flex)(props =>
+  css({
+    '> :first-child': { mb: 6 },
+    '> :nth-child(2)': { mb: 6 }
+  })
+);
+
+const Description = styled(Text)(props =>
+  css({ variant: 'text.introBannerDescription' })
+);
 
 const IntroBanner = () => (
-  <div>Evolving & incentivising collaboration across boundaries</div>
+  <Container flexDirection="column">
+    <IntroBannerHeading as="h1">
+      Evolving & incentivising collaboration across boundaries
+    </IntroBannerHeading>
+    <Description as="h2">
+      Create projects, collaborate, and get paid for doing great work in any
+      domain.
+    </Description>
+    <Button variant="secondary">Explore bounties</Button>
+  </Container>
 );
 
 export default IntroBanner;
--- src/theme.ts
+++ src/theme.ts
@@ -21,132 +21,6 @@ const aliases = ['sm', 'md', 'lg', 'xl'];
 addAliases(breakpoints, aliases);
 addAliases(mediaQueries, aliases);
 
-// SPACING SCALE
-export const space = [0, 4, 8, 16, 24, 32, 40, 128];
-export const sizes = [0, 4, 8, 16, 32, 40, 64, 80, 100, 128];
-
-// TYPOGRAPHY //
-export const font = '"Domine", Inter, -apple-system';
-export const fontMonospace = 'monospace';
-export const fontSizes = [12, 14, 16, 18, 20, 25];
-
-// Weights
-export const regular = 400;
-export const medium = 500;
-export const semiBold = 600;
-export const bold = 700;
-
-// styled-system's 'fontWeight' function can hook into the 'fontWeights' object
-export const fontWeights = {
-  regular,
-  medium,
-  semiBold,
-  bold
-};
-
-// Line-height
-export const lineHeights = {
-  h1: 37,
-  h2: 24,
-  h3: 22,
-  h4: 19,
-  h5: 17,
-  standard: 21,
-  small: 18,
-  reset: 1
-};
-
-// Scale
-export const text = {
-  h1: {
-    fontSize: fontSizes[5] + 'px',
-    fontWeight: fontWeights.bold,
-    lineHeight: lineHeights.h1 + 'px'
-  },
-  h2: {
-    fontSize: fontSizes[4] + 'px',
-    fontWeight: fontWeights.bold,
-    lineHeight: lineHeights.h2
-  },
-  h2Secondary: {
-    fontSize: fontSizes[4] + 'px',
-    fontWeight: fontWeights.medium,
-    lineHeight: lineHeights.h2
-  },
-  h3: {
-    fontSize: fontSizes[3] + 'px',
-    fontWeight: fontWeights.medium,
-    lineHeight: lineHeights.h3
-  },
-  h4: {
-    fontSize: fontSizes[2] + 'px',
-    fontWeight: fontWeights.medium,
-    lineHeight: lineHeights.h4
-  },
-  h5: {
-    fontSize: fontSizes[1] + 'px',
-    fontWeight: fontWeights.semiBold,
-    lineHeight: lineHeights.h5
-  },
-  bodyItalic: {
-    fontSize: fontSizes[1] + 'px',
-    fontWeight: fontWeights.regular,
-    fontStyle: 'italic',
-    lineHeight: lineHeights.standard
-  },
-  bodyStrong: {
-    fontSize: fontSizes[1] + 'px',
-    fontWeight: fontWeights.medium,
-    lineHeight: lineHeights.standard
-  },
-  body: {
-    fontSize: fontSizes[1] + 'px',
-    fontWeight: fontWeights.regular,
-    lineHeight: lineHeights.standard
-  },
-  smallStrong: {
-    fontSize: fontSizes[0] + 'px',
-    fontWeight: fontWeights.semiBold,
-    lineHeight: lineHeights.small
-  },
-  small: {
-    fontSize: fontSizes[0] + 'px',
-    fontWeight: fontWeights.regular,
-    lineHeight: lineHeights.small
-  },
-  link: {
-    fontSize: fontSizes[1] + 'px',
-    fontWeight: fontWeights.regular,
-    lineHeight: lineHeights.standard
-  },
-  linkStrong: {
-    fontSize: fontSizes[1] + 'px',
-    fontWeight: fontWeights.medium,
-    lineHeight: lineHeights.standard
-  },
-  linkSmall: {
-    fontSize: fontSizes[0] + 'px',
-    fontWeight: fontWeights.medium,
-    lineHeight: lineHeights.standard
-  },
-  label: {
-    fontSize: fontSizes[0] + 'px',
-    fontWeight: fontWeights.medium,
-    lineHeight: lineHeights.standard
-  },
-  help: {
-    fontSize: fontSizes[0] + 'px',
-    fontWeight: fontWeights.regular,
-    fontStyle: 'italic',
-    lineHeight: lineHeights.standard
-  },
-  tableHeading: {
-    fontSize: fontSizes[0] + 'px',
-    fontWeight: fontWeights.medium,
-    lineHeight: lineHeights.standard
-  }
-};
-
 // COLOR PALETTE
 const seaGlass100 = 'hsl(187, 45%, 81%)';
 const seaGlass200 = 'hsl(187, 50%, 60%)';
@@ -171,7 +45,7 @@ const gray200 = 'hsl(180, 2%, 91%)';
 const gray300 = 'hsl(199, 4%, 86%)';
 const gray400 = 'hsl(193, 4%, 57%)';
 const gray500 = 'hsl(199, 4%, 29%)';
-const black = 'hsl(193, 59%, 63%)';
+const black = 'hsl(197, 17%, 8%)';
 const white = 'hsl(0, 0%, 100%)';
 const transparentWhite = 'rgba(255, 255, 255, 65%)';
 
@@ -233,7 +107,46 @@ const colors = {
   baseBorderColor
 };
 
-export { colors };
+// SPACING SCALE
+export const space = [0, 4, 8, 16, 24, 32, 40, 128];
+export const sizes = [0, 4, 8, 16, 32, 40, 64, 80, 100, 128];
+
+// TYPOGRAPHY //
+export const body = '"Domine", Inter, -apple-system';
+export const heading = 'inherit';
+export const monospace = 'monospace';
+export const fontSizes = [12, 14, 16, 18, 20, 25];
+export const fonts = {
+  body,
+  heading,
+  monospace
+};
+
+// Weights
+export const regular = 400;
+export const medium = 500;
+export const semiBold = 600;
+export const bold = 700;
+
+// styled-system's 'fontWeight' function can hook into the 'fontWeights' object
+export const fontWeights = {
+  regular,
+  medium,
+  semiBold,
+  bold
+};
+
+// Line-height
+export const lineHeights = {
+  h1: 37,
+  h2: 24,
+  h3: 22,
+  h4: 19,
+  h5: 17,
+  standard: 21,
+  small: 18,
+  reset: 1
+};
 
 // BORDERS
 // styled-system's 'borderRadius' function can hook into the 'radii' object/array
@@ -241,10 +154,11 @@ export const radii = [0, 4, 8, '50%'];
 export const radius = '8px';
 
 const baseBorder = `1px solid ${colors.baseBorderColor}`;
+const activeBorder = `1px solid ${colors.gray300}`;
 const avatarBorder = `2px solid ${colors.white}`;
 const lightBorder = `1px solid ${colors.gray100}`;
 
-const borders = [baseBorder, avatarBorder, lightBorder];
+const borders = [baseBorder, activeBorder, avatarBorder, lightBorder];
 
 export { borders };
 
@@ -253,8 +167,9 @@ export const maxContainerWidth = '1280px';
 
 // BOX SHADOWS
 export const shadows = [
-  '0px 2px 4px rgba(17, 22, 24, 0.08);',
-  '0px 3px 5px rgba(17, 22, 24, 0.15);',
+  '0px 2px 4px rgba(17, 22, 24, 0.08);', //buttonShadow1, cardShadow
+  '0px 3px 5px rgba(17, 22, 24, 0.15);', //buttonShadow2
+  '0px 9px 24px rgba(0, 0, 0, 0.08)', //shadowLarge
   `0 0 10px ${colors.gray200}`,
   `inset 0 0 0 1px ${colors.brandSecondary}`,
   `inset 0 0 0 1px ${colors.brandDestructive}`
@@ -262,6 +177,137 @@ export const shadows = [
 
 //Z-INDEX
 export const zIndices = [0, 9, 99, 999, 9999];
+
+// Text variants
+export const text = {
+  h1: {
+    fontSize: fontSizes[5] + 'px',
+    fontWeight: fontWeights.bold,
+    lineHeight: lineHeights.h1 + 'px'
+  },
+  h2: {
+    fontSize: fontSizes[4] + 'px',
+    fontWeight: fontWeights.bold,
+    lineHeight: lineHeights.h2 + 'px'
+  },
+  h2Secondary: {
+    fontSize: fontSizes[4] + 'px',
+    fontWeight: fontWeights.medium,
+    lineHeight: lineHeights.h2 + 'px'
+  },
+  h3: {
+    fontSize: fontSizes[3] + 'px',
+    fontWeight: fontWeights.medium,
+    lineHeight: lineHeights.h3 + 'px'
+  },
+  h4: {
+    fontSize: fontSizes[2] + 'px',
+    fontWeight: fontWeights.medium,
+    lineHeight: lineHeights.h4 + 'px'
+  },
+  h5: {
+    fontSize: fontSizes[1] + 'px',
+    fontWeight: fontWeights.semiBold,
+    lineHeight: lineHeights.h5 + 'px'
+  },
+  bodyItalic: {
+    fontSize: fontSizes[1] + 'px',
+    fontWeight: fontWeights.regular,
+    fontStyle: 'italic',
+    lineHeight: lineHeights.standard + 'px'
+  },
+  bodyStrong: {
+    fontSize: fontSizes[1] + 'px',
+    fontWeight: fontWeights.medium,
+    lineHeight: lineHeights.standard + 'px'
+  },
+  body: {
+    fontSize: fontSizes[1] + 'px',
+    fontWeight: fontWeights.regular,
+    lineHeight: lineHeights.standard + 'px'
+  },
+  smallStrong: {
+    fontSize: fontSizes[0] + 'px',
+    fontWeight: fontWeights.semiBold,
+    lineHeight: lineHeights.small + 'px'
+  },
+  small: {
+    fontSize: fontSizes[0] + 'px',
+    fontWeight: fontWeights.regular,
+    lineHeight: lineHeights.small + 'px'
+  },
+  link: {
+    fontSize: fontSizes[1] + 'px',
+    fontWeight: fontWeights.regular,
+    lineHeight: lineHeights.standard + 'px'
+  },
+  linkStrong: {
+    fontSize: fontSizes[1] + 'px',
+    fontWeight: fontWeights.medium,
+    lineHeight: lineHeights.standard + 'px'
+  },
+  linkSmall: {
+    fontSize: fontSizes[0] + 'px',
+    fontWeight: fontWeights.medium,
+    lineHeight: lineHeights.standard + 'px'
+  },
+  label: {
+    fontSize: fontSizes[0] + 'px',
+    fontWeight: fontWeights.medium,
+    lineHeight: lineHeights.standard + 'px'
+  },
+  help: {
+    fontSize: fontSizes[0] + 'px',
+    fontWeight: fontWeights.regular,
+    fontStyle: 'italic',
+    lineHeight: lineHeights.standard + 'px'
+  },
+  tableHeading: {
+    fontSize: fontSizes[0] + 'px',
+    fontWeight: fontWeights.medium,
+    lineHeight: lineHeights.standard + 'px'
+  },
+  introBannerH1: {
+    fontFamily: 'Domine',
+    fontWeight: fontWeights.bold,
+    fontSize: '40px',
+    lineHeight: '51px',
+    color: colors.seaGlass400
+  },
+  introBannerDescription: {
+    fontFamily: 'Inter',
+    fontWeight: fontWeights.regular,
+    fontSize: fontSizes[2] + 'px',
+    lineHeight: lineHeights.h2 + 'px'
+  }
+};
+
+// Button variants
+export const buttons = {
+  secondary: {
+    background: 'white',
+    borderRadius: 2,
+    border: baseBorder,
+    color: colors.gray500,
+    width: '153px',
+    cursor: 'pointer',
+    ...text.bodyStrong,
+    ':hover': {
+      boxShadow: shadows[0],
+      color: colors.black,
+      border: borders[1]
+    },
+    ':active': {
+      color: colors.black,
+      border: borders[1]
+    },
+    ':disabled': {
+      background: colors.gray100,
+      cursor: 'not-allowed',
+      color: colors.gray300
+    }
+  }
+};
 
 //AVATAR STYLES
 export const avatarResourceTypes = {
@@ -288,22 +334,24 @@ const theme = {
   mediaQueries,
   space,
   sizes,
-  font,
+  fonts,
   fontSizes,
   fontWeights,
   lineHeights,
   regular,
   bold,
-  text,
   colors,
   radii,
   radius,
   borders,
   shadows,
   zIndices,
+  text,
+  buttons,
   avatarResourceTypes,
   textFormat,
   maxContainerWidth
 };
 
 export default theme;
+export { colors };
