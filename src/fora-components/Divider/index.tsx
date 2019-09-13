0b7cd342a26a18ff41a08b748ea9ef18e171b382
Eric Juta <ericjohn.juta@consensys.net>
2019-09-13 16:26:30 +0100

    Update Footer content

--- src/fora-components/Divider/index.tsx
+++ src/fora-components/Divider/index.tsx
@@ -2,18 +2,21 @@ import styled from 'lib/emotion-styled';
 import css from '@styled-system/css';
 import { Box } from 'rebass';
 
-const Divider = styled(Box)<{
-  mb?: number;
-  marginBottom?: number;
-  mt?: number;
-  marginTop?: number;
-}>(props =>
+interface IDividerProps {
+  backgroundColor?: string;
+  mb?: number | number[];
+  marginBottom?: number | number[];
+  mt?: number | number[];
+  marginTop?: number | number[];
+}
+
+const Divider = styled(Box)<IDividerProps>(props =>
   css({
-    mt: 3,
+    mt: props.mt || props.marginTop || 3,
     mb: props.mb || props.marginBottom || 3,
     width: '100%',
     height: '1px',
-    backgroundColor: 'gray200'
+    backgroundColor: props.backgroundColor || 'gray200'
   })
 );
 export default Divider;
