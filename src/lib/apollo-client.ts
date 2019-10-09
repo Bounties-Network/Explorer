5dbc6bbf1380f4ed6156d4be38ba113ea364cdbb
Eric Juta <ericjohn.juta@consensys.net>
2019-10-10 00:36:38 +0100

    Add Auth header

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -1,9 +1,16 @@
 import ApolloClient from 'apollo-boost';
+import cookie from 'cookie';
+
+const cookies = cookie.parse(document.cookie);
+const authCookie = cookies['Authorization'];
 
 const isStaging =
   typeof process.env.APP_SETTINGS_FILE === 'string' &&
   process.env.APP_SETTINGS_FILE.includes('staging');
 const client = new ApolloClient({
+  headers: {
+    Authorization: authCookie
+  },
   uri:
     typeof process.env.APP_SETTINGS_FILE === 'string' &&
     process.env.APP_SETTINGS_FILE.includes('local')
