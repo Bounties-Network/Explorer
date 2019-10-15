8175cd69846043af9ba5ff374cd15ac81abca3e5
Eric Juta <ericjohn.juta@consensys.net>
2019-10-15 12:56:06 +0100

    Remove unneeded console.logs

--- src/public-modules/Utilities/ipfsClient.js
+++ src/public-modules/Utilities/ipfsClient.js
@@ -1,28 +1,31 @@
-import ipfsMiniAPI from "ipfs-mini";
-import config from "public-modules/config";
+import ipfsMiniAPI from 'ipfs-mini';
+import config from 'public-modules/config';
 
 const ipfsConfig = config.ipfs;
 
-console.log(ipfsConfig);
+// console.log(ipfsConfig);
 const ipfsMini = new ipfsMiniAPI(ipfsConfig);
 
 export const addBufferToIPFS = (filename, bufferContent) =>
   new Promise((resolve, reject) => {
     // due to es5 issues - we load this via a CDN
-    console.log(ipfsConfig);
+    // console.log(ipfsConfig);
     const ipfs = window.IpfsApi(ipfsConfig);
 
-    ipfs.add([{ path: `/bounties/${filename}`, content: bufferContent }], (err, response) => {
-      if (err) {
-        reject(err);
+    ipfs.add(
+      [{ path: `/bounties/${filename}`, content: bufferContent }],
+      (err, response) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(response[1].hash);
       }
-      resolve(response[1].hash);
-    });
+    );
   });
 
 export const addJSON = data =>
   new Promise((resolve, reject) => {
-    console.log(ipfsConfig);
+    // console.log(ipfsConfig);
     ipfsMini.addJSON(data, (err, response) => {
       if (err) {
         reject(err);
