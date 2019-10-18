9eed0954b365034f1f1c2323d59b16c6762194f2
Eric Juta <ericjohn.juta@consensys.net>
2019-10-18 17:13:19 +0100

    Just use one theme provider

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,27 +1,24 @@
-require('@babel/register')({
-  extensions: ['.js', '.jsx', '.ts', '.tsx']
+require("@babel/register")({
+  extensions: [".js", ".jsx", ".ts", ".tsx"]
 });
 
-import React from 'react';
-import { configure, addDecorator } from '@storybook/react';
-import { ThemeProvider } from 'emotion-theming';
-import { ThemeProvider as CThemeProvider } from '@chakra-ui/core';
-import { Global } from '@emotion/core';
-import theme from '../src/theme';
-import globalStyles from 'styles/global-styles';
-import '../src/styles/flexboxgrid.css';
-import '../src/styles/index.scss';
-import '../src/styles/Toastify.scss';
+import React from "react";
+import { configure, addDecorator } from "@storybook/react";
+import { ThemeProvider } from "emotion-theming";
+import { Global } from "@emotion/core";
+import theme from "../src/theme";
+import globalStyles from "styles/global-styles";
+import "../src/styles/flexboxgrid.css";
+import "../src/styles/index.scss";
+import "../src/styles/Toastify.scss";
 
 addDecorator(story => (
   <ThemeProvider theme={theme}>
-    <CThemeProvider theme={theme}>
-      <React.Fragment>
-        <Global style={globalStyles} />
-        {story()}
-      </React.Fragment>
-    </CThemeProvider>
+    <React.Fragment>
+      <Global style={globalStyles} />
+      {story()}
+    </React.Fragment>
   </ThemeProvider>
 ));
 
-configure(require.context('../src/fora-components', true, /\.stories\.(js|mdx|tsx)$/), module);
+configure(require.context("../src/fora-components", true, /\.stories\.(js|mdx|tsx)$/), module);
