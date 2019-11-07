5e09e4057d611422b4a4a5d447c429666725a6a5
Eric Juta <ericjohn.juta@consensys.net>
2019-11-07 14:44:06 +0000

    Update url

--- src/public-modules/relayer-config.ts
+++ src/public-modules/relayer-config.ts
@@ -4,7 +4,7 @@ export default {
   relayerStagingContractAddress: '0x0d45e1f2cb1f28c05c9feee7d771a5acb2f237f5',
   relayerProductionContractAddress:
     '0xf7fc27202bc20ce95ef28340d8e542346cb56b6d',
-  relayerApiURL: `https://standardbounties-metatx-relayer.jx-${
+  relayerApiURL: `https://standardbounties-metatx-relayer-${
     moduleSettings.url.mainNet.includes('rinkeby') ||
     moduleSettings.url.mainNet.includes('staging')
       ? 'staging'
