95512da6adaf4d11b81459d7c1ccd89e0aecdb13
Eric Juta <ericjohn.juta@consensys.net>
2019-11-11 13:23:36 +0000

    Add dud addresses for bountiesMetaTxRelayer if below 2.2

--- src/public-modules/relayer-config.ts
+++ src/public-modules/relayer-config.ts
@@ -3,17 +3,36 @@ const moduleSettings = require(`./${process.env.APP_SETTINGS_FILE}.json`);
 export default {
   relayerAddresses: {
     mainNet: {
-      "bountiesMetaTxRelayerAddressV2.2": "0xf7fc27202bc20ce95ef28340d8e542346cb56b6d",
-      "bountiesMetaTxRelayerAddressV2.3": "0x7Da3F08b843029C323cCCeCef090d2F2C706ba42"
+      bountiesMetaTxRelayerAddressV1:
+        '0xf7fc27202bc20ce95ef28340d8e542346cb56b6d',
+      bountiesMetaTxRelayerAddressV2:
+        '0xf7fc27202bc20ce95ef28340d8e542346cb56b6d',
+      'bountiesMetaTxRelayerAddressV2.1':
+        '0xf7fc27202bc20ce95ef28340d8e542346cb56b6d',
+      // These above aren't used
+      'bountiesMetaTxRelayerAddressV2.2':
+        '0xf7fc27202bc20ce95ef28340d8e542346cb56b6d',
+      'bountiesMetaTxRelayerAddressV2.3':
+        '0x7Da3F08b843029C323cCCeCef090d2F2C706ba42'
     },
     rinkeby: {
-      "bountiesMetaTxRelayerAddressV2.2": "0x0d45e1f2cb1f28c05c9feee7d771a5acb2f237f5",
-      "bountiesMetaTxRelayerAddressV2.3": "0x70a1cd9b015253129b11ec9166beae620140b29d"
+      bountiesMetaTxRelayerAddressV1:
+        '0xf7fc27202bc20ce95ef28340d8e542346cb56b6d',
+      bountiesMetaTxRelayerAddressV2:
+        '0xf7fc27202bc20ce95ef28340d8e542346cb56b6d',
+      'bountiesMetaTxRelayerAddressV2.1':
+        '0xf7fc27202bc20ce95ef28340d8e542346cb56b6d',
+      // These above aren't used
+      'bountiesMetaTxRelayerAddressV2.2':
+        '0x0d45e1f2cb1f28c05c9feee7d771a5acb2f237f5',
+      'bountiesMetaTxRelayerAddressV2.3':
+        '0x70a1cd9b015253129b11ec9166beae620140b29d'
     }
   },
   relayerApiURL: `https://standardbounties-metatx-relayer-${
-    moduleSettings.url.mainNet.includes("rinkeby") || moduleSettings.url.mainNet.includes("staging")
-      ? "staging"
-      : "production"
+    moduleSettings.url.mainNet.includes('rinkeby') ||
+    moduleSettings.url.mainNet.includes('staging')
+      ? 'staging'
+      : 'production'
   }.bounties-network-flow.com`
 };
