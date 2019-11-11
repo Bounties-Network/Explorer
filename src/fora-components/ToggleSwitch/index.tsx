e5291acfe8fa68c80569bae5ab1a4938f32b54e1
Eric Juta <ericjohn.juta@consensys.net>
2019-11-11 14:18:14 +0000

    Fix TS child complaint

--- src/fora-components/ToggleSwitch/index.tsx
+++ src/fora-components/ToggleSwitch/index.tsx
@@ -1,22 +1,26 @@
 /** @jsx jsx */
-import { jsx } from 'theme-ui'
-import React from 'react'
-import { Switch } from '@chakra-ui/core' 
+import { jsx } from "theme-ui";
+import React from "react";
+import { Switch } from "@chakra-ui/core";
 
-const ToggleSwitch: React.FunctionComponent<{ isChecked, onChange: any, size?: string }> = (props) => (
-  <Switch {...props} sx={{
-    bg:
-      props.isChecked ? 'seaGlass300' : 'gray200',
-    boxShadow: 3, borderRadius: '100px',
-    '> *': { borderRadius: '100px' },
-    '> * > *': {
-      borderRadius: '50%',
-      mixBlendMode: 'normal', boxShadow: 0,
-      bg: 'white'
-    }
-  }}>
-
+const ToggleSwitch: React.FunctionComponent<{ isChecked; onChange: any; size?: string }> = props => (
+  <Switch
+    {...props}
+    sx={{
+      bg: props.isChecked ? "seaGlass300" : "gray200",
+      boxShadow: 3,
+      borderRadius: "100px",
+      "> *": { borderRadius: "100px" },
+      "> * > *": {
+        borderRadius: "50%",
+        mixBlendMode: "normal",
+        boxShadow: 0,
+        bg: "white"
+      }
+    }}
+  >
+    {props.children}
   </Switch>
-)
+);
 
-export default ToggleSwitch
+export default ToggleSwitch;
