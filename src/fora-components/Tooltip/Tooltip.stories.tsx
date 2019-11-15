238bd6172cf8cdb52a3d0bb2f57a20790dacb83f
Eric Juta <ericjohn.juta@consensys.net>
2019-11-15 16:44:41 +0000

    Tooltip

---
+++ src/fora-components/Tooltip/Tooltip.stories.tsx
@@ -0,0 +1,23 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+import React from 'react';
+import { storiesOf, addDecorator } from '@storybook/react';
+import centered from '@storybook/addon-centered/react';
+import Tooltip from '.';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faQuestionCircle } from "@fortawesome/pro-regular-svg-icons";
+
+addDecorator(centered);
+
+interface IContentProps {  }
+const Content: React.FC<IContentProps> = () => (<div>This is some helper text that you find inside of a tooltip. It might attempt to explain something to a user in unique instances where something may be unclear, or needs to be defined beyond how it is displayed in the UI.</div>)
+
+storiesOf('Tooltip', module)
+.add('Mi Fora', () => (
+  <Tooltip content={<Content />}>
+    <div sx={{ cursor: 'pointer' }}>
+      <FontAwesomeIcon icon={faQuestionCircle}></FontAwesomeIcon>
+    </div>
+  </Tooltip>
+  )
+)
---
+++ src/fora-components/Tooltip/index.tsx
@@ -0,0 +1,43 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+import React from "react";
+import Tippy from "@tippy.js/react";
+import { Global } from "@emotion/core";
+import css from "@styled-system/css";
+import theme from "theme";
+
+interface IProps {
+  content: React.ReactElement<any>;
+}
+
+const Tooltip: React.FunctionComponent<IProps> = props => (
+  <Tippy distance={8} trigger={"click"} interactive={true} content={props.content}>
+    <div>
+      <Global
+        styles={css({
+          ".tippy-tooltip": {
+            boxSizing: "border-box",
+            bg: "#F8FCFC",
+            borderRadius: 2,
+            boxShadow: "0px 2px 4px rgba(2, 75, 91, 0.1)"
+          },
+          ".tippy-arrow": { display: "none" }, // TODO: Never <_<
+          ".tippy-popper": {
+            background: "none"
+          },
+          ".tippy-content": {
+            ...theme.text.small,
+            fontWeight: "500",
+            color: "seaGlass300",
+            border: `1px solid hsl(187, 45%, 81%)`,
+            borderRadius: 2,
+            p: 3
+          }
+        })}
+      ></Global>
+      {props.children}
+    </div>
+  </Tippy>
+);
+
+export default Tooltip;
