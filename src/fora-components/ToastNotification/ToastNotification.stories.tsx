836aa4adc6e527882d021c71ef3d22b21400555c
Eric Juta <ericjohn.juta@consensys.net>
2019-11-17 21:30:19 +0000

    Checkin toast notification

--- src/fora-components/ToastNotification/ToastNotification.stories.tsx
+++ src/fora-components/ToastNotification/ToastNotification.stories.tsx
@@ -1,20 +1,14 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui";
-import React from 'react';
 import { storiesOf, addDecorator } from '@storybook/react';
 import centered from '@storybook/addon-centered/react';
 import { ToastContainer, ToastNotification, ToastNotificationContent } from '.';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faQuestionCircle, faTimes } from "@fortawesome/pro-regular-svg-icons";
 import { toast } from "react-toastify";
-import css from "@styled-system/css";
-import { useResponsiveValue } from '@theme-ui/match-media'
 
 addDecorator(centered);
 
 storiesOf('ToastNotification', module)
-.add('Mi Fora', () => {
-  const position = useResponsiveValue(['bottom-center', 'bottom-right', 'bottom-right'])
+.add('Generic title with detail and info type', () => {
 return (   <div>
   <ToastContainer />
   <div sx={{ cursor: 'pointer' }} onClick={() => toast.info(
@@ -25,7 +19,28 @@ return (   <div>
     detail={'Generic Subtext'}
     />,
     {
-      position,
+      position: 'bottom-right',
+      autoClose: 50000000000000,
+      hideProgressBar: false,
+      pauseOnHover: true,
+      draggable: true,
+    }
+    )
+  }>Click me</div>
+  </div> )
+  }
+)
+.add('Bounty received info', () => {
+return (   <div>
+  <ToastContainer />
+  <div sx={{ cursor: 'pointer' }} onClick={() => toast.info(
+    <ToastNotificationContent
+    type='info'
+    action={{  name: 'View Bounty', href: 'http://www.w3.org/2000/svg'  }}
+    title={'Your bounty received a submission'} 
+    />,
+    {
+      position: 'bottom-right',
       autoClose: 50000000000000,
       hideProgressBar: false,
       pauseOnHover: true,
--- src/fora-components/ToastNotification/index.tsx
+++ src/fora-components/ToastNotification/index.tsx
@@ -5,7 +5,7 @@ import { Global } from "@emotion/core";
 import css from "@styled-system/css";
 import theme from "theme";
 import { Flex, Text, Link, Button } from "rebass";
-import { faInfoCircle, faTimes, faExclamationCircle, faRampLoading } from "@fortawesome/pro-regular-svg-icons";
+import { faInfoCircle, faTimes, faExclamationCircle, faRampLoading } from "@fortawesome/pro-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { toast, ToastContainer as RTToastContainer } from "react-toastify";
 import { faCheckCircle } from "@fortawesome/pro-light-svg-icons";
@@ -14,13 +14,16 @@ interface IProps {
   content: React.ReactElement<any>;
 }
 
+const width = 430
+const height = 80
+
 interface IToastContainerProps {  }
 export const ToastContainer: React.FC<IToastContainerProps> = (props) => (
   <Fragment>
     <RTToastContainer  toastClassName='Fora-toast--notification' />
     <Global
       styles={css({
-        ".Fora-toast--notification": { bg: "white", border: "base", boxShadow: 2, borderRadius: 2 }
+        ".Fora-toast--notification": { bg: "white", border: "base", boxShadow: 2, borderRadius: 2, width, marginLeft: (width / 4) * -1, height },
       })}
     />
   </Fragment>
@@ -30,17 +33,18 @@ type NotificationType = "info" | "success" | "warning" | "error" | 'loading'
 
 interface IToastNotificationContentProps {
   action?: { name: string; href: string };
-  title: string;
+  title: string | React.ReactNode;
   detail?: string;
   type: NotificationType;
 }
 
+const typeIconStyle = { color: "seaGlass200", mr: 3 }
 export const ToastNotificationContent: React.FC<IToastNotificationContentProps> = props => (
     <Flex alignItems="center" flexDirection="row" sx={{ px: 2, position: 'relative' }}>
-      {props.type === "info" && <FontAwesomeIcon sx={{ color: "seaGlass200", mr: 3 }} icon={faInfoCircle}></FontAwesomeIcon>}
-      {props.type === "success" && <FontAwesomeIcon sx={{ color: "seaGlass200", mr: 3 }} icon={faCheckCircle}></FontAwesomeIcon>}
-      {props.type === "loading" && <FontAwesomeIcon sx={{ color: "seaGlass200", mr: 3 }} icon={faRampLoading}></FontAwesomeIcon>}
-      {props.type === "error" && <FontAwesomeIcon sx={{ color: "seaGlass200", mr: 3 }} icon={faExclamationCircle}></FontAwesomeIcon>}
+      {props.type === "info" && <FontAwesomeIcon sx={typeIconStyle} size={'lg'} icon={faInfoCircle}></FontAwesomeIcon>}
+      {props.type === "success" && <FontAwesomeIcon sx={typeIconStyle} size={'lg'} icon={faCheckCircle}></FontAwesomeIcon>}
+      {props.type === "loading" && <FontAwesomeIcon sx={typeIconStyle} size={'lg'} icon={faRampLoading}></FontAwesomeIcon>}
+      {props.type === "error" && <FontAwesomeIcon sx={typeIconStyle} size={'lg'} icon={faExclamationCircle}></FontAwesomeIcon>}
       <Flex flexDirection="column">
         <Text variant="bodyStrong" color="black">
           {props.title}
@@ -49,7 +53,7 @@ export const ToastNotificationContent: React.FC<IToastNotificationContentProps> 
           {props.detail}
         </Text>
       </Flex>
-      <Flex sx={{ ml: 'auto' }}>
+      <Flex sx={{ ml: 'auto', minWidth: '80px', justifyContent: 'flex-end' }}>
         <Link href={props?.action?.href} variant="link">
           {props?.action?.name}
         </Link>
@@ -57,8 +61,8 @@ export const ToastNotificationContent: React.FC<IToastNotificationContentProps> 
     <FontAwesomeIcon sx={{
       color: 'gray300',
       position: 'absolute',
-    top: "-4px",
-    right: '-8px'
+      top: "-16px",
+      right: '-8px'
 }} icon={faTimes}></FontAwesomeIcon>
     </Flex>
 );
