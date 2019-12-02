348a300990a1a5d39d558d1adba5b53da2a6fa6f
Eric Juta <ericjohn.juta@consensys.net>
2019-12-02 14:40:44 +0000

    Fix Tabs

--- src/fora-components/Tabs/index.tsx
+++ src/fora-components/Tabs/index.tsx
@@ -11,21 +11,21 @@ import css from "@styled-system/css";
 const TabList = emotionStyled(CTabList)(props =>
   css({
     border: "none",
-    borderBottom: `1px solid ${props.theme.colors.gray["200"]}`,
+    borderBottom: `1px solid ${props.theme.colors.brandGray["200"]}`,
 
     button: {
       border: "none",
       background: "none",
-      color: props.theme.colors.gray["400"],
+      color: props.theme.colors.brandGray["400"],
       borderBottom: `2px solid transparent`,
       boxSizing: "border-box",
-      ...props.theme.text.h5,
-      fontWeight: props.theme.fontWeights.regular
+      ...props.theme.text.bodyStrong,
+      fontWeight: props.theme.fontWeights.normal
     },
 
     "button[aria-selected=true]": {
       border: "none",
-      borderBottom: `2px solid ${props.theme.colors.seaGlass["400"]}`,
+      borderBottom: `2px solid ${props.theme.colors.brandPrimary["400"]}`,
       boxSizing: "border-box",
       color: "black",
       fontWeight: props.theme.fontWeights.medium
