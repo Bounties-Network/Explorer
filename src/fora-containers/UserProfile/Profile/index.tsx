4befcf85b6a9be2a52e8dfb57cdeb00eceebbbf9
Eric Juta <ericjohn.juta@consensys.net>
2019-12-17 11:07:53 +0000

    Check in 2

--- src/fora-containers/UserProfile/Profile/index.tsx
+++ src/fora-containers/UserProfile/Profile/index.tsx
@@ -9,19 +9,55 @@ import Pill from "fora-components/Pill";
 import { shortenAddress } from "utils/helpers";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBriefcase, faComments, faGlobe } from "@fortawesome/pro-regular-svg-icons";
+import { faTwitter, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 interface IProps {}
 
 const SkillPill = ({ category }) => (
-  <Pill variant="tag.explorer">
+  <Pill shape="square" variant="tag.explorer">
     <Text color={"brandGray.400"} variant="body">
       {category}
     </Text>
   </Pill>
 );
 
+const ElsewhereLink = ({ icon, href, linkText }) => (
+  <Flex sx={{ alignItems: "center", '> svg:first-of-type': { mr: 1 } }}>
+    <FontAwesomeIcon sx={{ color: "brandGray.400" }} icon={icon}></FontAwesomeIcon>
+    <Link href={href} variant="text.link">
+      {linkText}
+    </Link>
+  </Flex>
+);
+
+const profileChildSpacing = {
+  1: 5,
+  2: 3,
+  3: 4,
+  4: 4,
+  5: 1,
+  6: 4,
+  7: 4
+};
+const nthChild = (x: string) => `> *:nth-child(${x})`;
+const profileChildSpacingStyles = Object.keys(profileChildSpacing).reduce((current, next) => {
+  if (profileChildSpacing[next]) {
+    const lol = {
+      [nthChild(next)]: { mb: profileChildSpacing[next] }
+    };
+    return Object.assign(current, lol);
+  }
+  return current;
+}, {});
+
+const profileContainerStyle = {
+  flexDirection: "column",
+  mt: -75,
+  ...profileChildSpacingStyles
+};
+
 const Profile: React.FunctionComponent<IProps> = props => (
-  <Flex sx={{ flexDirection: "column", mt: -75 }}>
+  <Flex sx={profileContainerStyle}>
     <AvatarImage
       size="large"
       src={
@@ -33,7 +69,9 @@ const Profile: React.FunctionComponent<IProps> = props => (
     <Text variant="headingSerif" sx={{ fontSize: "h2" }}>
       Simona Pop
     </Text>
-    <Pill size="large" variant="status.affirmative">{shortenAddress("0xbfeceC47dD8bf5F6264A9830A9d26ef387c38A67")}</Pill>
+    <Pill size="large" variant="status.affirmative">
+      {shortenAddress("0xbfeceC47dD8bf5F6264A9830A9d26ef387c38A67")}
+    </Pill>
     <Text variant="body">
       Imagination is everything. Co-Founder & CMO of Bounties Network and fora at ConsenSys. Queen bee of the hive. You
       test me I sting you.
@@ -52,11 +90,11 @@ const Profile: React.FunctionComponent<IProps> = props => (
       </Text>
     </Flex>
 
-    <Flex sx={{ flexDirection: "column" }}>
+    <Flex sx={{ flexDirection: "column", "> div:first-of-type": { mb: 2 } }}>
       <Text variant="bodyStrong" sx={{ fontSize: "h5" }}>
         Skills
       </Text>
-      <Flex sx={{ flexWrap: 1, "> *:not(:last-of-type)": { mr: 1 } }}>
+      <Flex sx={{ flexWrap: "wrap", minWidth: "250px", m: -1, "> *": { m: 1 } }}>
         <SkillPill category="React"></SkillPill>
         <SkillPill category="CSS"></SkillPill>
         <SkillPill category="HTML"></SkillPill>
@@ -65,29 +103,14 @@ const Profile: React.FunctionComponent<IProps> = props => (
       </Flex>
     </Flex>
 
-    <Flex sx={{ flexDirection: "column" }}>
+    <Flex sx={{ flexDirection: "column", "> div:first-of-type": { mb: 2 } }}>
       <Text variant="bodyStrong" sx={{ fontSize: "h5" }}>
         Elsewhere
       </Text>
       <Flex sx={{ flexDirection: "column", "> *:not(:last-of-type)": { mb: 2 } }}>
-        <Flex sx={{ alignItems: "center" }}>
-          <FontAwesomeIcon sx={{ color: "brandGray.400" }} icon={faGlobe}></FontAwesomeIcon>
-          <Link href="https://www.google.com" variant="text.link">
-            firstNameLastName.com
-          </Link>
-        </Flex>
-        <Flex sx={{ alignItems: "center" }}>
-          <FontAwesomeIcon sx={{ color: "brandGray.400" }} icon={faGlobe}></FontAwesomeIcon>
-          <Link href="https://www.google.com" variant="text.link">
-            @twitterUsername
-          </Link>
-        </Flex>
-        <Flex sx={{ alignItems: "center" }}>
-          <FontAwesomeIcon sx={{ color: "brandGray.400" }} icon={faGlobe}></FontAwesomeIcon>
-          <Link href="https://www.google.com" variant="text.link">
-            @githubusername
-          </Link>
-        </Flex>
+        <ElsewhereLink icon={faGlobe} href={"www.google.co.uk"} linkText="firstNameLastName.com"></ElsewhereLink>
+        <ElsewhereLink icon={faTwitter} href={"www.google.co.uk"} linkText="@twitterUsername"></ElsewhereLink>
+        <ElsewhereLink icon={faGithub} href={"www.google.co.uk"} linkText="@githubUsername"></ElsewhereLink>
       </Flex>
     </Flex>
   </Flex>
